299294eb20a0c3bc5bb687d528b78af22c125ceb
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2019-10-23 18:23:47 +0100

    added colorbar to overlap matrix plot, and vmin vmax params for normalised overlap matrices

--- utils/custom_functions.py
+++ utils/custom_functions.py
@@ -1,16 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def visualise_matrix(matrix_data: np.ndarray, fig_title: str=None):
+def visualise_matrix(matrix_data: np.ndarray, fig_title: str=None, normalised: bool=True):
     """
     Show heatmap of matrix
 
     :param matrix data: (M x N) numpy array containing matrix data
     :param fig_title: title to be given to figure
+    :param normalised: whether or not matrix data is normalised
     :return fig: matplotlib figure with visualisation of matrix data
     """
     fig = plt.figure()
-    plt.imshow(matrix_data)
+    if normalised:
+        plt.imshow(matrix_data, vmin=0, vmax=1)
+    else:
+        plt.imshow(matrix_data)
+    plt.colorbar()
     if fig_title:
         fig.suptitle(fig_title, fontsize=20)
     plt.close()
