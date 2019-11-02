2737aed88771c4931b4a8ab1ea1da5fc87d695b7
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2019-11-02 20:29:50 +0000

    added ability to copy x percent of weights per layer to each teacher and have rest of weights be random i.e. overlap x percent of weights for each teacher

--- frameworks/base_frameworks/overlapping_teachers.py
+++ frameworks/base_frameworks/overlapping_teachers.py
@@ -1,6 +1,8 @@
 from models import Teacher, MetaStudent, ContinualStudent
 
-from frameworks.teacher_student import StudentTeacher
+from frameworks import StudentTeacher
+
+import torch 
 
 import copy
 
@@ -27,8 +29,16 @@ class OverlappingTeachers(StudentTeacher):
         original_teacher.set_noise_distribution(mean=0, std=teacher_noises[0])
         self.teachers.append(original_teacher)
         for t in range(self.num_teachers - 1):
-            teacher = copy.deepcopy(original_teacher)
-
+            teacher = Teacher(config=config).to(self.device)
+            teacher.freeze_weights()
+            teacher.set_noise_distribution(mean=0, std=teacher_noises[0])
+            for l, layer in enumerate(teacher.state_dict()):
+                layer_shape = teacher.state_dict()[layer].shape 
+                assert len(layer_shape) == 2, "shape of layer tensor is not 2. Check consitency of layer construction with task."
+                for row in range(layer_shape[0]):
+                    overlapping_weights_dim = round(0.01 * overlap_percentage * layer_shape[1])
+                    overlapping_weights = copy.deepcopy(original_teacher.state_dict()[layer][row][:overlapping_weights_dim])
+                    teacher.state_dict()[layer][row][:overlapping_weights_dim] = overlapping_weights
             self.teachers.append(teacher)
 
     def _signal_task_boundary_to_teacher(self, new_task: int):
