168ed3bf2446480fa6083fefaf252cd7da907f48
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2019-11-03 22:01:53 +0000

    towards defining noise in terms of range of teacher outputs

--- models/teachers/teacher.py
+++ models/teachers/teacher.py
@@ -11,12 +11,24 @@ class Teacher(Model):
     def __init__(self, config: Dict) -> None:
 
         Model.__init__(self, config=config, model_type='teacher')
+        self.noisy = False
+
+    def get_output_statistics(self, repeats=1000):
+        with torch.no_grad():
+            random_input = torch.randn(repeats, self.input_dimension)
+            output = self.forward(random_input)
+            output_mean = float(torch.mean(output))
+            output_std = float(torch.std(output))
+            output_max = float(max(output))
+            output_min = float(min(output))
+            output_range = output_max - output_min
 
     def set_noise_distribution(self, mean: float, std: float):
         """
         Sets a normal distribution from which to sample noise that is added to output
         """
         self.noise_distribution = tdist.Normal(torch.Tensor([mean]), torch.Tensor([std]))
+        self.noisy = True
 
     def _construct_output_layers(self):
         self.output_layer = nn.Linear(self.hidden_dimensions[-1], self.output_dimension, bias=self.bias)
@@ -27,7 +39,7 @@ class Teacher(Model):
 
     def _output_forward(self, x: torch.Tensor) -> torch.Tensor:
         y = self.output_layer(x)
-        if self.noise_distribution:
+        if self.noisy:
             noise = self.noise_distribution.sample((y.shape[0],))
             y = y + noise
         return y
