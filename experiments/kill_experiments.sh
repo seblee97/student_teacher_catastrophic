691077f53de2d102bc12a3b030c75892fdb6af0e
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2019-11-03 08:53:19 +0000

    script to kill all tmux shells/experiments

---
+++ experiments/kill_experiments.sh
@@ -0,0 +1,10 @@
+tmux kill-session -t meta_independent
+tmux kill-session -t meta_noisy
+tmux kill-session -t meta_drifting
+tmux kill-session -t meta_overlapping
+tmux kill-session -t continual_independent
+tmux kill-session -t continual_noisy
+tmux kill-session -t continual_drifting
+tmux kill-session -t continual_overlapping
+
+tmux kill-session -t tensorboard
