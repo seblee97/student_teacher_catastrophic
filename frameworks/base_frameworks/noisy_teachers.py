f1e11149719a18247a4c9890a389cc9c16e0db4e
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2019-11-03 22:14:01 +0000

    added noise dist based on output std

--- frameworks/base_frameworks/noisy_teachers.py
+++ frameworks/base_frameworks/noisy_teachers.py
@@ -19,8 +19,14 @@ class NoisyTeachers(StudentTeacher):
         self.teachers = []
         base_teacher = Teacher(config=config).to(self.device)
         base_teacher.freeze_weights()
+        base_teacher_output_std = base_teacher.get_output_statistics()
         for t in range(self.num_teachers):
             teacher = copy.deepcopy(base_teacher)
-            teacher.set_noise_distribution(mean=0, std=teacher_noises[t])
+            teacher.set_noise_distribution(mean=0, std=teacher_noises[t] * base_teacher_output_std)
             self.teachers.append(teacher)
     
+    def _signal_task_boundary_to_teacher(self, new_task: int):
+        pass
+
+    def _signal_step_boundary_to_teacher(self, step: int, current_task: int):
+        pass
