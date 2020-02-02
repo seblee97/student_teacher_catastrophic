563da26fbf841f9e54296342a3752f247d05a0ca
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-02-02 14:53:07 +0000

    added mnist to frameworks import and added mnist to learners imports

--- frameworks/__init__.py
+++ frameworks/__init__.py
@@ -1,4 +1,6 @@
-from .base_frameworks.teacher_student import StudentTeacher
+from .base_frameworks.teacher_student import StudentTeacher, MNIST
 # from .base_frameworks.noisy_teachers import NoisyTeachers
 # from .base_frameworks.independent_teachers import IndependentTeachers
-from .learners import MetaNoisy, MetaIndependent, ContinualNoisy, ContinualIndependent, MetaDrifting, ContinualDrifting, MetaOverlapping, ContinualOverlapping
+from .learners import MetaNoisy, MetaIndependent, ContinualNoisy, \
+    ContinualIndependent, MetaDrifting, ContinualDrifting, MetaOverlapping, \
+    ContinualOverlapping, ContinualMNIST, MetaMNIST
