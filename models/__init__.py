ef9fbee6ba4874b1d94e915bd751ff6a73b2f2c6
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-02-02 14:56:29 +0000

    minor: import statements of new learners etc.

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 from .base_network import Model
-from .students.meta_student import MetaStudent
-from .students.continual_student import ContinualStudent
+from .students.meta_student import MetaStudent, MNISTMetaStudent
+from .students.continual_student import ContinualStudent, MNISTContinualStudent
 from .teachers.teacher import Teacher
 from .teachers.drifting_teacher import DriftingTeacher
