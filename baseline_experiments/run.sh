c6cfbe525aff20d1c4b13227e317340221d76866
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-02-23 21:47:34 +0000

    minor: added space(typos)

--- baseline_experiments/run.sh
+++ baseline_experiments/run.sh
@@ -1,12 +1,12 @@
 # regression
-python main.py -config configs/regression/relu_relu_big/base_config.yaml--ac additional_configs/  --en regression_relu_relu_big
-python main.py -config configs/regression/relu_relu_small/base_config.yaml--ac additional_configs/ --en regression_relu_relu_small
-python main.py -config configs/regression/relu_sigmoid_big/base_config.yaml--ac additional_configs/ --en regression_relu_sigmoid_big
-python main.py -config configs/regression/relu_sigmoid_small/base_config.yaml--ac additional_configs/ --en regression_relu_sigmoid_small
-python main.py -config configs/regression/sigmoid_relu_big/base_config.yaml--ac additional_configs/ --en regression_sigmoid_relu_big
-python main.py -config configs/regression/sigmoid_relu_small/base_config.yaml--ac additional_configs/ --en regression_sigmoid_relu_small
-python main.py -config configs/regression/sigmoid_sigmoid_big/base_config.yaml--ac additional_configs/ --en regression_sigmoid_sigmoid_big
-python main.py -config configs/regression/sigmoid_sigmoid_small/base_config.yaml--ac additional_configs/ --en regression_sigmoid_sigmoid_small
+python main.py -config configs/regression/relu_relu_big/base_config.yaml --ac additional_configs/  --en regression_relu_relu_big
+python main.py -config configs/regression/relu_relu_small/base_config.yaml --ac additional_configs/ --en regression_relu_relu_small
+python main.py -config configs/regression/relu_sigmoid_big/base_config.yaml --ac additional_configs/ --en regression_relu_sigmoid_big
+python main.py -config configs/regression/relu_sigmoid_small/base_config.yaml --ac additional_configs/ --en regression_relu_sigmoid_small
+python main.py -config configs/regression/sigmoid_relu_big/base_config.yaml --ac additional_configs/ --en regression_sigmoid_relu_big
+python main.py -config configs/regression/sigmoid_relu_small/base_config.yaml --ac additional_configs/ --en regression_sigmoid_relu_small
+python main.py -config configs/regression/sigmoid_sigmoid_big/base_config.yaml --ac additional_configs/ --en regression_sigmoid_sigmoid_big
+python main.py -config configs/regression/sigmoid_sigmoid_small/base_config.yaml --ac additional_configs/ --en regression_sigmoid_sigmoid_small
 
 # classification
 python main.py -config configs/classification/relu_relu_big/base_config.yaml --ac additional_configs/ --en clssification_relu_relu_big
