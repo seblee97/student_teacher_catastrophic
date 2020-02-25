d69dba6a67fa1a31218bc481fa3e226fc074baf2
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-02-25 13:19:59 +0000

    simplified run commands

--- baseline_experiments/run.sh
+++ baseline_experiments/run.sh
@@ -1,43 +1,27 @@
 # regression
-python main.py -config configs/regression/relu_relu_big/base_config.yaml --ac additional_configs/  --en regression_relu_relu_big
-python main.py -config configs/regression/relu_relu_small/base_config.yaml --ac additional_configs/ --en regression_relu_relu_small
-python main.py -config configs/regression/relu_sigmoid_big/base_config.yaml --ac additional_configs/ --en regression_relu_sigmoid_big
-python main.py -config configs/regression/relu_sigmoid_small/base_config.yaml --ac additional_configs/ --en regression_relu_sigmoid_small
-python main.py -config configs/regression/sigmoid_relu_big/base_config.yaml --ac additional_configs/ --en regression_sigmoid_relu_big
-python main.py -config configs/regression/sigmoid_relu_small/base_config.yaml --ac additional_configs/ --en regression_sigmoid_relu_small
-python main.py -config configs/regression/sigmoid_sigmoid_big/base_config.yaml --ac additional_configs/ --en regression_sigmoid_sigmoid_big
-python main.py -config configs/regression/sigmoid_sigmoid_small/base_config.yaml --ac additional_configs/ --en regression_sigmoid_sigmoid_small
+python main.py -config base_config.yaml --ac additional_configs/ --en regression_relu_relu_small --lty regression --snl relu --tnl relu --lr 0.2
+python main.py -config base_config.yaml --ac additional_configs/ --en regression_relu_sigmoid_small --lty regression --snl relu --tnl sigmoid --lr 0.2
+python main.py -config base_config.yaml --ac additional_configs/ --en regression_sigmoid_relu_small --lty regression --snl sigmoid --tnl relu --lr 0.2
+python main.py -config base_config.yaml --ac additional_configs/ --en regression_sigmoid_sigmoid_small --lty regression --snl sigmoid --tnl sigmoid --lr 0.2
 
 # classification
-python main.py -config configs/classification/relu_relu_big/base_config.yaml --ac additional_configs/ --en clssification_relu_relu_big
-python main.py -config configs/classification/relu_relu_small/base_config.yaml --ac additional_configs/ --en clssification_relu_relu_small
-python main.py -config configs/classification/relu_sigmoid_big/base_config.yaml --ac additional_configs/ --en clssification_relu_sigmoid_big
-python main.py -config configs/classification/relu_sigmoid_small/base_config.yaml --ac additional_configs/ --en clssification_relu_sigmoid_small
-python main.py -config configs/classification/sigmoid_relu_big/base_config.yaml --ac additional_configs/ --en clssification_sigmoid_relu_big
-python main.py -config configs/classification/sigmoid_relu_small/base_config.yaml --ac additional_configs/ --en clssification_sigmoid_relu_small
-python main.py -config configs/classification/sigmoid_sigmoid_big/base_config.yaml --ac additional_configs/ --en clssification_sigmoid_sigmoid_big
-python main.py -config configs/classification/sigmoid_sigmoid_small/base_config.yaml --ac additional_configs/ --en clssification_sigmoid_sigmoid_small
+python main.py -config base_config.yaml --ac additional_configs/ --en clssification_relu_relu_small --lty classification --snl relu --tnl relu --lr 0.2
+python main.py -config base_config.yaml --ac additional_configs/ --en clssification_relu_sigmoid_small --lty classification --snl relu --tnl sigmoid --lr 0.2
+python main.py -config base_config.yaml --ac additional_configs/ --en clssification_sigmoid_relu_small --lty classification --snl sigmoid --tnl sigmoid --lr 0.2
+python main.py -config base_config.yaml --ac additional_configs/ --en clssification_sigmoid_sigmoid_small --lty classification --snl sigmoid --tnl sigmoid --lr 0.2
 
 # mnist-regression
-python main.py -config configs/mnist_regression/relu_relu_big/base_config.yaml --ac additional_configs/ --en mnist_regression_relu_relu_big
-python main.py -config configs/mnist_regression/relu_relu_small/base_config.yaml --ac additional_configs/ --en mnist_regression_relu_relu_small
-python main.py -config configs/mnist_regression/relu_sigmoid_big/base_config.yaml --ac additional_configs/ --en mnist_regression_relu_sigmoid_big
-python main.py -config configs/mnist_regression/relu_sigmoid_small/base_config.yaml --ac additional_configs/ --en mnist_regression_relu_sigmoid_small
-python main.py -config configs/mnist_regression/sigmoid_relu_big/base_config.yaml --ac additional_configs/ --en mnist_regression_sigmoid_relu_big
-python main.py -config configs/mnist_regression/sigmoid_relu_small/base_config.yaml --ac additional_configs/ --en mnist_regression_sigmoid_relu_small
-python main.py -config configs/mnist_regression/sigmoid_sigmoid_big/base_config.yaml --ac additional_configs/ --en mnist_regression_sigmoid_sigmoid_big
-python main.py -config configs/mnist_regression/sigmoid_sigmoid_small/base_config.yaml --ac additional_configs/ --en mnist_regression_sigmoid_sigmoid_small
+python main.py -config base_config.yaml --ac additional_configs/ --en mnist_regression_relu_relu_small --lty regression --snl relu --tnl relu --lr 0.2
+python main.py -config base_config.yaml --ac additional_configs/ --en mnist_regression_relu_sigmoid_small --lty regression --snl relu --tnl sigmoid --lr 0.2
+python main.py -config base_config.yaml --ac additional_configs/ --en mnist_regression_sigmoid_relu_small --lty regression --snl sigmoid --tnl relu --lr 0.2
+python main.py -config base_config.yaml --ac additional_configs/ --en mnist_regression_sigmoid_sigmoid_small --lty regression --snl sigmoid --tnl sigmoid --lr 0.2
 
 # mnist-classification
-python main.py -config configs/mnist_classification/relu_relu_big/base_config.yaml --ac additional_configs/ --en mnist_classification_relu_relu_big
-python main.py -config configs/mnist_classification/relu_relu_small/base_config.yaml --ac additional_configs/ --en mnist_classification_relu_relu_small
-python main.py -config configs/mnist_classification/relu_sigmoid_big/base_config.yaml --ac additional_configs/ --en mnist_classification_relu_sigmoid_big
-python main.py -config configs/mnist_classification/relu_sigmoid_small/base_config.yaml --ac additional_configs/ --en mnist_classification_relu_sigmoid_small
-python main.py -config configs/mnist_classification/sigmoid_relu_big/base_config.yaml --ac additional_configs/ --en mnist_classification_sigmoid_relu_big
-python main.py -config configs/mnist_classification/sigmoid_relu_small/base_config.yaml --ac additional_configs/ --en mnist_classification_sigmoid_relu_small
-python main.py -config configs/mnist_classification/sigmoid_sigmoid_big/base_config.yaml --ac additional_configs/ --en mnist_classification_sigmoid_sigmoid_big
-python main.py -config configs/mnist_classification/sigmoid_sigmoid_small/base_config.yaml --ac additional_configs/ --en mnist_classification_sigmoid_sigmoid_small
+python main.py -config base_config.yaml --ac additional_configs/ --en mnist_classification_relu_relu_small --lty classification --snl relu --tnl relu --lr 0.2
+python main.py -config base_config.yaml --ac additional_configs/ --en mnist_classification_relu_sigmoid_small --lty classification --snl relu --tnl sigmoid --lr 0.2
+python main.py -config base_config.yaml --ac additional_configs/ --en mnist_classification_sigmoid_relu_small --lty classification --snl sigmoid --tnl relu --lr 0.2
+python main.py -config base_config.yaml --ac additional_configs/ --en mnist_classification_sigmoid_sigmoid_small --lty classification --snl sigmoid --tnl sigmoid --lr 0.2
 
 # pure-mnist
-python main.py -config configs/pure_mnist/relu/base_config.yaml --ac additional_configs/ --en pure_mnist_classification_relu
-python main.py -config configs/pure_mnist/sigmoid/base_config.yaml --ac additional_configs/ --en pure_mnist_classification_sigmoid
+python main.py -config base_config.yaml --ac additional_configs/ --en pure_mnist_classification_relu --lty not_applicable --snl relu --tnl relu --lr 0.2
+python main.py -config base_config.yaml --ac additional_configs/ --en pure_mnist_classification_sigmoid --lty not_applicable --snl sigmoid --tnl sigmoid --lr 0.2
