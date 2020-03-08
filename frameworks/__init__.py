b1c46ac9dfb56a57cf98691d62f039d3877c58cb
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-03-08 15:03:00 +0000

    student teacher runner to package init - all else removed

--- frameworks/__init__.py
+++ frameworks/__init__.py
@@ -1,6 +1,1 @@
-from .base_frameworks.teacher_student import StudentTeacher, MNIST
-# from .base_frameworks.noisy_teachers import NoisyTeachers
-# from .base_frameworks.independent_teachers import IndependentTeachers
-from .learners import MetaNoisy, MetaIndependent, ContinualNoisy, \
-    ContinualIndependent, MetaDrifting, ContinualDrifting, MetaOverlapping, \
-    ContinualOverlapping, ContinualMNIST, MetaMNIST
+from .teacher_student import StudentTeacherRunner
