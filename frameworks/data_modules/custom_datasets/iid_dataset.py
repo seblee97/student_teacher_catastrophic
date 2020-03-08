1c4de76606ea93cb602e48c83dfe05f20eab7292
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-03-08 23:36:37 +0000

    custom torch dataset for iid data. Not currently using this but may come in handy

---
+++ frameworks/data_modules/custom_datasets/iid_dataset.py
@@ -0,0 +1,33 @@
+from torch.utils.data import Dataset
+
+import torch
+
+import numpy as np
+
+class IIDDataset(Dataset):
+    """IID gaussian samples dataset."""
+
+    def __init__(self, dimension: int, num_datapoints: int, repeat: bool=False):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+            on a sample.
+        """
+        self.dimension = dimension
+        self.repeat = repeat
+        
+        self._dataset = torch.randn(num_datapoints, self.dimension)
+
+        self.dataset_looped = False
+
+    def __len__(self):    
+        return len(self._dataset)
+
+    def __getitem__(self, idx):
+        if idx == 0 and !(self.dataset_looped):
+            self.dataset_looped = True    
+        elif idx == 0 and !(self.repeat) and self.dataset_looped:
+            raise IterationError("This IID dataset has already been looped through but repeat is off. Please check")
+        return self._dataset[idx]
