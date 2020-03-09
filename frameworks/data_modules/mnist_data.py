c3c75cae1fdec4546c8e4a880c504383ede37820
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-03-09 14:17:01 +0000

    docstrings, type hinting, removed redundant imports, made class variables 'private' where appropriate

--- frameworks/data_modules/mnist_data.py
+++ frameworks/data_modules/mnist_data.py
@@ -1,4 +1,4 @@
-from .base_data_module import BaseData
+from .base_data_module import _BaseData
 
 from typing import Dict
 
@@ -7,23 +7,24 @@ import os
 import torch
 import torchvision 
 
-from utils import custom_torch_transforms
+from utils import custom_torch_transforms, get_pca
 
-class MNISTData(BaseData):
+class MNISTData(_BaseData):
+
+    """Class for dealing with data generated from MNIST images"""
 
     def __init__(self, config: Dict):
-        BaseData.__init__(self, config)
+        _BaseData.__init__(self, config)
 
-        self.train_batch_size = config.get(["training", "train_batch_size"])
-        self.pca_input = config.get(["training", "pca_input"])
-        self.data_path = config.get("data_path")
+        self._pca_input = config.get(["training", "pca_input"])
+        self._data_path = config.get("data_path")
 
         self._load_mnist_data()
 
-    def _load_mnist_data(self):
+    def _load_mnist_data(self) -> None:
 
         file_path = os.path.dirname(os.path.realpath(__file__))
-        full_data_path = os.path.join(file_path, self.data_path)
+        full_data_path = os.path.join(file_path, self._data_path)
 
         # transforms to add to data
         transform_list = [
@@ -35,14 +36,15 @@ class MNISTData(BaseData):
 
         base_transform = torchvision.transforms.Compose(transform_list)
 
+        # load dataset to compute channel statistics
         mnist_train_data = torchvision.datasets.MNIST(full_data_path, transform=base_transform, train=True)
         mnist_train_dataloader = torch.utils.data.DataLoader(mnist_train_data, batch_size=60000)
 
+        # evaluate channel mean and std
         data_mu = torch.mean(next(iter(mnist_train_dataloader))[0], axis=0)
         data_sigma = torch.std(next(iter(mnist_train_dataloader))[0], axis=0)
 
-        # normalise_transform = torchvision.transforms.Normalize(data_mu, data_sigma)
-        # transform_list.insert(0, normalise_transform)
+        # add normalisation to transforms
         normalise_transform = custom_torch_transforms.Standardize(data_mu, data_sigma)
         transform_list.append(normalise_transform)
 
@@ -63,16 +65,28 @@ class MNISTData(BaseData):
         self.mnist_train_data = torchvision.datasets.MNIST(full_data_path, transform=transform, train=True)
         self.mnist_test_data = torchvision.datasets.MNIST(full_data_path, transform=transform, train=False)
 
-        self.training_dataloader = torch.utils.data.DataLoader(self.mnist_train_data, batch_size=self.train_batch_size, shuffle=True)
+        self.training_dataloader = torch.utils.data.DataLoader(self.mnist_train_data, batch_size=self._train_batch_size, shuffle=True)
         self.training_data_iterator = iter(self.training_dataloader)
 
-        self.test_dataloader = torch.utils.data.DataLoader(self.mnist_test_data, batch_size=self.test_batch_size)
+        self.test_dataloader = torch.utils.data.DataLoader(self.mnist_test_data, batch_size=self._test_batch_size)
 
-    def get_test_set(self):
+    def get_test_set(self) -> List[torch.Tensor, torch.Tensor]:
+        """
+        returns fixed test data set (data and labels)
+        
+        return data: test data inputs
+        return labels: corresponding labels for test data inputs
+        """
         data, labels = next(iter(self.test_dataloader))
         return data, labels
 
-    def get_batch(self):
+    def get_batch(self) -> torch.Tensor:
+        """
+        returns batch of training data. Retrieves next batch from dataloader iterator.
+        If iterator is empty, it is reset.
+
+        return batch_input:  
+        """
         try:
             batch_input = next(self.training_data_iterator)[0]
         except:
