178fca63d2f77d20393540ea9509625106624549
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-03-09 16:57:04 +0000

    changed to inherit from base teacher

--- frameworks/teachers/drifting_teachers.py
+++ frameworks/teachers/drifting_teachers.py
@@ -1,27 +1,34 @@
 from models import DriftingTeacher
 
-from frameworks import StudentTeacher
+from .base_teacher import _BaseTeacher
 
-class DriftingTeachers(StudentTeacher):
+import torch 
+import copy
 
-    def __init__(self, config):
-        StudentTeacher.__init__(self, config)
-        self.drift_frequency = config.get(["task", "drift_frequency"])
+from typing import Dict
 
-    def _setup_teachers(self, config):
+class DriftingTeachers(_BaseTeacher):
+
+    def __init__(self, config: Dict):
+        _BaseTeacher.__init__(self, config)
+        self._drift_frequency = config.get(["task", "drift_frequency"])
+
+    def _setup_teachers(self, config: Dict):
         """Instantiate Teachers"""
         # initialise teacher networks, freeze
         teacher_noise = config.get(["task", "teacher_noise"])
+
         if type(teacher_noise) is int:
-            teacher_noises = [teacher_noise for _ in range(self.num_teachers)]
+            teacher_noises = [teacher_noise for _ in range(self._num_teachers)]
+
         elif type(teacher_noise) is list:
-            assert len(teacher_noise) == self.num_teachers, \
-            "Provide one noise for each teacher. {} noises given, {} teachers specified".format(len(teacher_noise), self.num_teachers)
+            assert len(teacher_noise) == self._num_teachers, \
+            "Provide one noise for each teacher. {} noises given, {} teachers specified".format(len(teacher_noise), self._num_teachers)
             teacher_noises = teacher_noise
 
         self.teachers = []
-        for t in range(self.num_teachers):
-            teacher = DriftingTeacher(config=config, index=t).to(self.device)
+        for t in range(self._num_teachers):
+            teacher = DriftingTeacher(config=config, index=t).to(self._device)
             teacher.freeze_weights()
             if teacher_noises[t] != 0:
                 teacher_output_std = teacher.get_output_statistics()
@@ -32,8 +39,6 @@ class DriftingTeachers(StudentTeacher):
         pass
 
     def _signal_step_boundary_to_teacher(self, step: int, current_task: int):
-        self.teachers[current_task].forward_count += 1
-        if step % self.drift_frequency == 0:
-            self.teachers[current_task].rotate_weights()
-
-    
+        self._teachers[current_task].forward_count += 1
+        if step % self._drift_frequency == 0:
+            self._teachers[current_task].rotate_weights()
