7bb206add486c5982f84b0ead7deb1cf604d3713
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-03-09 16:52:48 +0000

    docstrings, type hinting and variable to class variables

--- frameworks/teachers/base_teacher.py
+++ frameworks/teachers/base_teacher.py
@@ -1,38 +1,45 @@
 from abc import ABC, abstractmethod
 
-from typing import Dict
+import torch
+import torch.nn as nn
+
+from typing import Dict, List
 
 class _BaseTeacher(ABC):
 
     def __init__(self, config: Dict):
 
-        self.num_teachers = config.get(["task", "num_teachers"])
-        self.device = config.get("device")
+        self._num_teachers = config.get(["task", "num_teachers"])
+        self._device = config.get("device")
 
         self._setup_teachers(config=config)
 
     @abstractmethod
-    def _setup_teachers(self, config):
+    def _setup_teachers(self, config: Dict) -> None:
+        """instantiate teacher network(s)"""
         raise NotImplementedError("Base class method")
 
-    def forward(self, teacher_index: int, x):
+    def forward(self, teacher_index: int, x: torch.Tensor) -> torch.Tensor:
+        """call to current teacher forward"""
         output = self._teachers[teacher_index](x)
         return output
 
-    def forward_all(self, x):
+    def forward_all(self, x: torch.Tensor) -> List[torch.Tensor]:
+        """call to forward of all teachers (used primarily for evaluation)"""
         outputs = [teacher(x) for teacher in self._teachers]
         return outputs
 
-    def get_teacher_networks(self):
+    def get_teacher_networks(self) -> List[nn.Module]:
+        """getter method for teacher networks"""
         networks = self._teachers 
         return networks
 
     @abstractmethod
-    def signal_task_boundary_to_teacher(self, new_task: int):
+    def signal_task_boundary_to_teacher(self, new_task: int) -> None:
         raise NotImplementedError("Base class method")
 
     @abstractmethod
-    def signal_step_boundary_to_teacher(self, step: int, current_task: int):
+    def signal_step_boundary_to_teacher(self, step: int, current_task: int) -> None:
         raise NotImplementedError("Base class method")
 
 
