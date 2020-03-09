b4eab6974b003bedf66a21527642592b3a6fac49
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-03-09 14:42:59 +0000

    fixed use of this class for new interface, added type hinting and removed redundant code

--- frameworks/learners/meta_learner.py
+++ frameworks/learners/meta_learner.py
@@ -1,29 +1,20 @@
-from models import Teacher, MetaStudent, ContinualStudent, MNISTContinualStudent, MNISTMetaStudent
+from models import MetaStudent
 
-from frameworks import StudentTeacher, MNIST
+from .base_learner import _BaseLearner
 
-import copy
+from typing import Dict
 
-import torch
-
-class MetaLearner(StudentTeacher):
+class MetaLearner(_BaseLearner):
 
     def __init__(self, config):
-        StudentTeacher.__init__(self, config)
+        _BaseLearner.__init__(self, config)
 
-    def _setup_student(self, config):
+    def _setup_student(self, config: Dict) -> None:
         """Instantiate student"""
-        # initialise student network
-        self.student_network = MetaStudent(config=config).to(self.device)
+        self._student_network = MetaStudent(config=config).to(self._device)
 
-    def _signal_task_boundary_to_learner(self, new_task: int):
-        self.student_network.set_task(new_task)
+    def signal_task_boundary_to_learner(self, new_task: int) -> None:
+        self._student_network.set_task(new_task)
 
-    def _signal_step_boundary_to_learner(self, step: int, current_task: int):
+    def signal_step_boundary_to_learner(self, step: int, current_task: int) -> None:
         pass
-
-    def _compute_generalisation_errors(self, teacher_index=None):
-        with torch.no_grad():
-            student_outputs = self.student_network.test_all_tasks(self.test_input_data)
-            generalisation_errors = [float(self._compute_loss(student_outputs, teacher_output)) for teacher_output in self.test_teacher_outputs]
-            return {'generalisation_error': generalisation_errors}
