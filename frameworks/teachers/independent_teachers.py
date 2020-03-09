73b0d0200ab2ce6f810c3c75fd14d6448705a788
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-03-09 16:58:52 +0000

    updated interface to inherit from bas teachers

--- frameworks/teachers/independent_teachers.py
+++ frameworks/teachers/independent_teachers.py
@@ -1,31 +1,33 @@
 from models import Teacher
 
-from frameworks import StudentTeacher
+from .base_teacher import _BaseTeacher
 
-class IndependentTeachers(StudentTeacher):
+from typing import Dict
 
-    def __init__(self, config):
-        StudentTeacher.__init__(self, config)
+class IndependentTeachers(_BaseTeacher):
 
-    def _setup_teachers(self, config):
+    def __init__(self, config: Dict):
+        _BaseTeacher.__init__(self, config)
+
+    def _setup_teachers(self, config: Dict) -> None:
         """Instantiate all teachers"""
         # initialise teacher networks, freeze
         teacher_noise = config.get(["task", "teacher_noise"])
         if type(teacher_noise) is int:
-            teacher_noises = [teacher_noise for _ in range(self.num_teachers)]
+            teacher_noises = [teacher_noise for _ in range(self._num_teachers)]
         elif type(teacher_noise) is list:
-            assert len(teacher_noise) == self.num_teachers, \
-            "Provide one noise for each teacher. {} noises given, {} teachers specified".format(len(teacher_noise), self.num_teachers)
+            assert len(teacher_noise) == self._num_teachers, \
+            "Provide one noise for each teacher. {} noises given, {} teachers specified".format(len(teacher_noise), self._num_teachers)
             teacher_noises = teacher_noise
 
-        self.teachers = []
-        for t in range(self.num_teachers):
-            teacher = Teacher(config=config, index=t).to(self.device)
+        self._teachers = []
+        for t in range(self._num_teachers):
+            teacher = Teacher(config=config, index=t).to(self._device)
             teacher.freeze_weights()
             if teacher_noises[t] != 0:
                 teacher_output_std = teacher.get_output_statistics()
                 teacher.set_noise_distribution(mean=0, std=teacher_noises[t] * teacher_output_std)
-            self.teachers.append(teacher)
+            self._teachers.append(teacher)
 
     def _signal_task_boundary_to_teacher(self, new_task: int):
         pass
