8a695cb0da08ff339594aefe2e54310d0d228c80
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-03-09 16:38:43 +0000

    removed redundant code. Note currently only base class is used

--- frameworks/loss_modules/regression_loss.py
+++ frameworks/loss_modules/regression_loss.py
@@ -6,6 +6,4 @@ class RegressionLoss(_BaseLoss):
 
     def __init__(self, config: Dict):
         _BaseLoss.__init__(self, config=config)
-
-    # def compute_loss(self, prediction, target):
-    #     return self.loss_function(prediction, target)
+        
