e185cea8968e5a8896aa815822b6abcc57543b77
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-03-09 16:34:38 +0000

    some variables changed to private

--- frameworks/loggers/student_teacher_logger.py
+++ frameworks/loggers/student_teacher_logger.py
@@ -30,23 +30,23 @@ class StudentTeacherLogger(_BaseLogger):
             layer = layer + "_head_{}".format(str(head))
 
         # compute overlap matrices
-        student_self_overlap = (student_layer.mm(student_layer.t()) / self.input_dimension).cpu().numpy()
+        student_self_overlap = (student_layer.mm(student_layer.t()) / self._input_dimension).cpu().numpy()
         if head is None:
-            student_teacher_overlaps = [(student_layer.mm(teacher_layer.t()) / self.input_dimension).cpu().numpy() for teacher_layer in teacher_layers]
+            student_teacher_overlaps = [(student_layer.mm(teacher_layer.t()) / self._input_dimension).cpu().numpy() for teacher_layer in teacher_layers]
         else:
-            student_teacher_overlaps = [(student_layer.t().mm(teacher_layer) / self.input_dimension).cpu().numpy() for teacher_layer in teacher_layers]
-        teacher_self_overlaps = [(teacher_layer.mm(teacher_layer.t()) / self.input_dimension).cpu().numpy() for teacher_layer in teacher_layers]
+            student_teacher_overlaps = [(student_layer.t().mm(teacher_layer) / self._input_dimension).cpu().numpy() for teacher_layer in teacher_layers]
+        teacher_self_overlaps = [(teacher_layer.mm(teacher_layer.t()) / self._input_dimension).cpu().numpy() for teacher_layer in teacher_layers]
         teacher_pairs = list(itertools.combinations(range(len(teacher_layers)), 2))
-        teacher_teacher_overlaps = {(i, j): (teacher_layers[i].mm(teacher_layers[j].t()) / self.input_dimension).cpu().numpy() for (i, j) in teacher_pairs}
+        teacher_teacher_overlaps = {(i, j): (teacher_layers[i].mm(teacher_layers[j].t()) / self._input_dimension).cpu().numpy() for (i, j) in teacher_pairs}
 
         # log overlap values (scalars vs image graphs below)
         def log_matrix_values(log_name: str, matrix):
             matrix_shape = matrix.shape
             for i in range(matrix_shape[0]):
                 for j in range(matrix_shape[1]):
-                    if self.verbose_tb:
+                    if self._verbose_tb:
                         self._writer.add_scalar("layer_{}_{}/values_{}_{}".format(layer, log_name, i, j), matrix[i][j], step_count)
-                    if self.log_to_df:
+                    if self._log_to_df:
                         self._logger_df.at[step_count, "layer_{}_{}/values_{}_{}".format(layer, log_name, i, j)] = matrix[i][j]
         
         log_matrix_values("student_self_overlap", student_self_overlap)
@@ -58,7 +58,7 @@ class StudentTeacherLogger(_BaseLogger):
             log_matrix_values("teacher_teacher_overlaps/{}_{}".format(i, j), teacher_teacher_overlaps[(i, j)])
 
         # generate visualisations
-        if self.verbose_tb:
+        if self._verbose_tb:
             student_self_fig = visualise_matrix(student_self_overlap, fig_title=r"$Q_{ik}^\mu$")
             teacher_cross_figs = {(i, j):
                 visualise_matrix(teacher_teacher_overlaps[(i, j)], fig_title=r"$T_{nm}$") \
@@ -74,7 +74,7 @@ class StudentTeacherLogger(_BaseLogger):
             ]
 
         # log visualisations
-        if self.verbose_tb:
+        if self._verbose_tb:
             self._writer.add_figure("layer_{}_student_self_overlap".format(str(layer)), student_self_fig, step_count)
             for t, student_teacher_fig in enumerate(student_teacher_figs):
                 self._writer.add_figure("layer_{}_student_teacher_overlaps/teacher_{}".format(layer, t), student_teacher_fig, step_count)
