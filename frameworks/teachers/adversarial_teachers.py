6c3130b70a2b70a41a412f3bcd51c20670aec0ff
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-03-09 16:55:16 +0000

    changed to new interface for teachers. The adversarial class is still not properly implemented -see docstring of class

--- frameworks/teachers/adversarial_teachers.py
+++ frameworks/teachers/adversarial_teachers.py
@@ -1,16 +1,16 @@
 from models import Teacher
 
-from frameworks import StudentTeacher
+from .base_teacher import _BaseTeacher
 
 import torch.optim as optim
 
-class AdversarialTeachers(StudentTeacher):
+class AdversarialTeachers(_BaseTeacher):
 
     def __init__(self, config):
         raise NotImplementedError("Unsure how to supervise teacher i.e. should it know about scores from student and be trained to maximise student error?")
-        StudentTeacher.__init__(self, config)
+        _BaseTeacher.__init__(self, config)
 
-        trainable_parameters = [filter(lambda param: param.requires_grad, teacher.parameters()) for teacher in self.teachers]
+        trainable_parameters = [filter(lambda param: param.requires_grad, teacher.parameters()) for teacher in self._teachers]
         self.teacher_optimisers = [optim.SGD(teacher_parameters, lr=self.learning_rate) for teacher_parameters in trainable_parameters]
 
     def _setup_teachers(self, config):
@@ -20,12 +20,12 @@ class AdversarialTeachers(StudentTeacher):
             teacher_noises = [teacher_noise for _ in range(self.num_teachers)]
         elif type(teacher_noise) is list:
             assert len(teacher_noise) == self.num_teachers, \
-            "Provide one noise for each teacher. {} noises given, {} teachers specified".format(len(teacher_noise), self.num_teachers)
+            "Provide one noise for each teacher. {} noises given, {} teachers specified".format(len(teacher_noise), self._num_teachers)
             teacher_noises = teacher_noise
 
         self.teachers = []
-        for t in range(self.num_teachers):
-            teacher = Teacher(config=config, index=t).to(self.device)
+        for t in range(self._num_teachers):
+            teacher = Teacher(config=config, index=t).to(self._device)
             if teacher_noises[t] != 0:
                 teacher.set_noise_distribution(mean=0, std=teacher_noises[t])
             self.teachers.append(teacher)
@@ -35,5 +35,3 @@ class AdversarialTeachers(StudentTeacher):
 
     def _signal_step_boundary_to_teacher(self, step: int, current_task: int):
         pass
-
-        
