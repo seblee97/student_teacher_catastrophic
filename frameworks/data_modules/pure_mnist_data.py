8e2f27feceade2a3b143e92236052823c91005a8
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-03-21 09:29:18 +0000

    added loading of targets

--- frameworks/data_modules/pure_mnist_data.py
+++ frameworks/data_modules/pure_mnist_data.py
@@ -1,18 +1,22 @@
 from .mnist_data import _MNISTData 
 
-from typing import Dict
+from typing import Dict, List
 
 import torch
 import torchvision
 from torch.utils.data import Subset, ConcatDataset, DataLoader
 
+import numpy as np
+
 class PureMNISTData(_MNISTData):
 
     def __init__(self, config: Dict):
-        _MNISTData.__init__(self, config)
 
         self._mnist_teacher_classes = config.get(['training', 'teachers'])
         self._current_teacher_index = None
+        self._num_teachers = config.get(["task", "num_teachers"])
+
+        _MNISTData.__init__(self, config)
 
         if self._num_teachers != len(self._mnist_teacher_classes):
             raise AssertionError("Number of teachers specified in base config is not compatible with\
@@ -24,7 +28,7 @@ class PureMNISTData(_MNISTData):
         full_test_data = torchvision.datasets.MNIST(self._full_data_path, transform=self.transform, train=False)
 
         full_train_dataset_targets = full_train_data.targets
-        full_test_dataset_targets = full_train_data.targets
+        full_test_dataset_targets = full_test_data.targets
 
         def filter_dataset_by_target(target_to_keep: int, train: bool):
             """generate subset of dataset filtered by target"""
@@ -56,9 +60,8 @@ class PureMNISTData(_MNISTData):
 
             task_train_datasets.append(train_task_dataset)
             task_test_datasets.append(test_task_dataset)
-        
-        # test dataset consists of test images of all classes covered in training
-        self.test_data = ConcatDataset(task_test_datasets)
+
+        self.task_test_dataloaders = [DataLoader(task_test_data, batch_size=len(task_test_data)) for task_test_data in task_test_datasets]
 
         self.task_train_dataloaders = [DataLoader(task_train_data, batch_size=self._train_batch_size, shuffle=True) for task_train_data in task_train_datasets]
         self.task_train_iterators = [iter(training_dataloader) for training_dataloader in self.task_train_dataloaders]
@@ -68,7 +71,11 @@ class PureMNISTData(_MNISTData):
         # self.training_dataloader = torch.utils.data.DataLoader(self.mnist_train_data, batch_size=self._train_batch_size, shuffle=True)
         # self.training_data_iterator = iter(self.training_dataloader)
 
-        # self.test_dataloader = torch.utils.data.DataLoader(self.mnist_test_data, batch_size=self._test_batch_size)
+    def get_test_set(self) -> (torch.Tensor, List[torch.Tensor]):
+        test_sets = [next(iter(test_dataloader)) for test_dataloader in self.task_test_dataloaders]
+        data = torch.cat([test_set[0] for test_set in test_sets])
+        labels = [test_set[1] for test_set in test_sets]
+        return data, labels
     
     def get_batch(self) -> Dict[str, torch.Tensor]:
         """
@@ -81,7 +88,7 @@ class PureMNISTData(_MNISTData):
             batch = next(self.task_train_iterators[self._current_teacher_index])
         except:
             self.task_train_iterators[self._current_teacher_index] = iter(self.task_train_dataloaders[self._current_teacher_index])
-            batch = next(self.training_data_iterator)[0]
+            batch = next(self.training_data_iterator)
 
         return {'x': batch[0], 'y': batch[1]}
 
