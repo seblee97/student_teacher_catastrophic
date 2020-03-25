e10f30408869b8372c1fb9ab28a233e54c8ebd76
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-03-25 21:45:56 +0000

    utils/train_mnist_classifier.py

--- utils/train_mnist_classifier.py
+++ utils/train_mnist_classifier.py
@@ -28,9 +28,15 @@ class ClassificationTeacher(Model):
         y = self.output_layer(x)
         
         sigmoid_y = torch.sigmoid(y)
-        negative_class_probabilities = 1 - sigmoid_y
-        log_softmax = torch.log(torch.cat((negative_class_probabilities, sigmoid_y), axis=1))
-        return log_softmax
+        # negative_class_probabilities = 1 - sigmoid_y
+
+        return sigmoid_y
+        
+        # print(negative_class_probabilities)
+        # return torch.cat((negative_class_probabilities, sigmoid_y), dim=1)
+        # log_softmax = torch.log(torch.cat((negative_class_probabilities, sigmoid_y), dim=1))
+        # # import pdb; pdb.set_trace()
+        # return log_softmax
 
 class MNISTTrainer:
 
@@ -48,22 +54,33 @@ class MNISTTrainer:
         self.lr = config.get(["trained_mnist", "learning_rate"])
 
         self.optimiser = torch.optim.Adam(self.model.parameters(), lr=self.lr)
-        self.loss_function = nn.CrossEntropyLoss()
+        self.loss_function = nn.BCELoss()
 
     def train(self):
 
         running_mean_loss = deque([np.inf], maxlen=10)
 
+        # import pdb; pdb.set_trace()
+        b = 0
+        s = 0
+
         while np.mean(running_mean_loss) > self.convergence_criterion:
+
+            b += 32
+            s += 1
+
+            # if s == 370:
+            #     import pdb; pdb.set_trace()
 
             batch = self.data_module.get_batch() # returns dictionary 
 
             batch_input = batch.get('x')
-            batch_labels = batch.get('y') # returns None unless using pure MNIST teachers
+            batch_labels = batch.get('y').type(torch.FloatTensor) # returns None unless using pure MNIST teachers
 
-            predictions = self.model(batch_input)
+            predictions = self.model(batch_input).squeeze()
 
             self.optimiser.zero_grad()
+            # import pdb; pdb.set_trace()
             loss = self.loss_function(predictions, batch_labels)
             loss.backward()
             self.optimiser.step()
@@ -71,7 +88,7 @@ class MNISTTrainer:
             mean_loss = float(torch.mean(loss))
             running_mean_loss.append(mean_loss)
 
-            print(np.mean(running_mean_loss))
+            print(b, s, np.mean(running_mean_loss), [w.grad for w in self.model.parameters()][0].sum())
 
     def save_model_weights(self, path: str):
         torch.save(self.model.state_dict(), path)
