7785358a864a26f39b7e9a6758d1586543939183
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-05-08 19:14:33 +0100

    removed frameworks and added relevant units from models and components

--- experiments/context.py
+++ experiments/context.py
@@ -3,4 +3,5 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import utils
-import frameworks
+from models import learners, teachers
+from components import data_modules, loss_modules, loggers
