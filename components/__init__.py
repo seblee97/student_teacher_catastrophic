52e9dd2fc2171cc1b488fa021225cf8160287a2d
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-05-13 14:18:40 +0100

    pure -> digits, added even/greater class

--- components/__init__.py
+++ components/__init__.py
@@ -1,3 +1,3 @@
 from .loss_modules import ClassificationLoss, RegressionLoss
 from .loggers import StudentMNISTLogger, StudentTeacherLogger
-from .data_modules import IIDData, MNISTStreamData, PureMNISTData
+from .data_modules import IIDData, MNISTStreamData, MNISTDigitsData, MNISTEvenGreaterData 
