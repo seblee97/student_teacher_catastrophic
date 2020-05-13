b1e6663051cb6054bccd1d343696365861fcfc2a
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-05-13 10:41:44 +0100

    removed input source (moved to data) and test batch size (moved to tresting)

--- experiments/config_templates/training_template.py
+++ experiments/config_templates/training_template.py
@@ -15,10 +15,6 @@ class TrainingTemplate(utils._Template):
     TRAIN_BATCH_SIZE = utils.Field(
         name="train_batch_size", types=(int), reqs=[lambda x: x > 0]
     )
-    
-    TEST_BATCH_SIZE = utils.Field(
-        name="test_batch_size", types=(int), reqs=[lambda x: x > 0]
-    )
 
     LEARNING_RATE = utils.Field(
         name="learning_rate", types=(float, int), reqs=[lambda x: x > 0]
@@ -32,10 +28,6 @@ class TrainingTemplate(utils._Template):
         name="loss_function", types=(str), reqs=[lambda x: x in ["mse", "bce"]]
     )
 
-    INPUT_SOURCE = utils.Field(
-        name="input_source", types=(str), reqs=[lambda x: x in ["mnist", "iid_gaussian"]]
-    )
-
     SCALE_OUTPUT_BACKWARD = utils.Field(
         name="scale_output_backward", types=(bool), reqs=None
     )
@@ -45,10 +37,8 @@ class TrainingTemplate(utils._Template):
         return [
             cls.TOTAL_TRAINING_STEPS,
             cls.TRAIN_BATCH_SIZE,
-            cls.TEST_BATCH_SIZE,
             cls.LEARNING_RATE,
             cls.WEIGHT_DECAY,
             cls.LOSS_FUNCTION,
-            cls.INPUT_SOURCE,
             cls.SCALE_OUTPUT_BACKWARD
         ]
