e17d9d12358aa711132ea150bf0c6eefe53a0a18
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-05-14 17:09:05 +0100

    added mnist and general base data classes (mostly for type hinting purposes)

--- components/data_modules/__init__.py
+++ components/data_modules/__init__.py
@@ -2,3 +2,5 @@ from .iid_data import IIDData
 from .mnist_stream_data import MNISTStreamData
 from .mnist_digits_data import MNISTDigitsData
 from .even_greater_mnist_data import MNISTEvenGreaterData
+from .mnist_data import _MNISTData
+from .base_data_module import _BaseData
