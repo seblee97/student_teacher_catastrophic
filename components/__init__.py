9c28af1dd1eb24643dc8ad577e8f870c0fe51354
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-05-14 17:06:14 +0100

    added submodules to init of components

--- components/__init__.py
+++ components/__init__.py
@@ -1,3 +1,5 @@
 from .loss_modules import ClassificationLoss, RegressionLoss
 from .loggers import StudentMNISTLogger, StudentTeacherLogger
 from .data_modules import IIDData, MNISTStreamData, MNISTDigitsData, MNISTEvenGreaterData 
+
+# from . import data_modules, loggers, loss_modules
