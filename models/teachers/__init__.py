75a965d0f41233388aab354f8e3e67844676cd31
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-05-14 17:25:54 +0100

    teachers base class added

--- models/teachers/__init__.py
+++ models/teachers/__init__.py
@@ -1,3 +1,4 @@
 from .overlapping_teachers import OverlappingTeachers
 from .pure_mnist_teachers import PureMNISTTeachers
 from .trained_mnist_teachers import TrainedMNISTTeachers
+from .base_teachers import _BaseTeachers
