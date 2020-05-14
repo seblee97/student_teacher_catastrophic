e2da9aae9c5c48e43c344689dce7bb8525b66902
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-05-14 17:17:33 +0100

    removed context files, moved to approach of absolute imports (requires adding project path to pythonpath)

--- experiments/context.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-import utils
-from models import learners, teachers
-from components import data_modules, loss_modules, loggers
-from postprocessing import StudentTeacherPostprocessor
