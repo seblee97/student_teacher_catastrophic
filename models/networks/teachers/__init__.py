a27acc51de43fa15f6eaaabcebd036f1793d4e00
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-05-14 17:22:57 +0100

    added base teacher network

--- models/networks/teachers/__init__.py
+++ models/networks/teachers/__init__.py
@@ -1,3 +1,4 @@
 from .classification_teacher import ClassificationTeacher 
 from .regression_teacher import RegressionTeacher
 from .trained_classification_teacher import TrainedClassificationTeacher
+from .base_teacher import _Teacher
