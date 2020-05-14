271d9247536afa8179e2bc4b0bbfa62b4748354e
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-05-14 17:23:32 +0100

    mypy typing in particular overloading for test set forward method - this whoel stuff should be reconsidered and made clearner

--- models/teachers/base_teachers.py
+++ models/teachers/base_teachers.py
@@ -3,13 +3,14 @@ from abc import ABC, abstractmethod
 import torch
 import torch.nn as nn
 
-from typing import Dict, List
+from typing import Dict, List, Union, overload
 
-from models.networks.teachers import ClassificationTeacher, RegressionTeacher, TrainedClassificationTeacher
+from models.networks.teachers import ClassificationTeacher, RegressionTeacher, TrainedClassificationTeacher, _Teacher
+from utils import Parameters
 
 class _BaseTeachers(ABC):
 
-    def __init__(self, config: Dict):
+    def __init__(self, config: Parameters):
 
         self._num_teachers = config.get(["task", "num_teachers"])
         self._device = config.get("device")
@@ -19,12 +20,14 @@ class _BaseTeachers(ABC):
 
         self._setup_teachers(config=config)
 
+        self._teachers: List[_Teacher]
+
     @abstractmethod
-    def _setup_teachers(self, config: Dict) -> None:
+    def _setup_teachers(self, config: Parameters) -> None:
         """instantiate teacher network(s)"""
         raise NotImplementedError("Base class method")
 
-    def _init_teacher(self, config: Dict, index: int):
+    def _init_teacher(self, config: Parameters, index: int):
         if self._loss_type == "classification":
             if self._teacher_configuration == "trained_mnist":
                 return TrainedClassificationTeacher(config=config, index=index).to(self._device)
@@ -33,21 +36,29 @@ class _BaseTeachers(ABC):
         elif self._loss_type == "regression":
             return RegressionTeacher(config=config, index=index).to(self._device)
 
+    @overload
     @abstractmethod
-    def test_set_forward(self, teacher_index: int, batch: torch.Tensor) -> torch.Tensor:
+    def test_set_forward(self, teacher_index: int, batch: List[Dict[str, torch.Tensor]]) -> torch.Tensor: ...
+        
+    @overload
+    @abstractmethod
+    def test_set_forward(self, teacher_index: int, batch: Dict[str, Union[torch.Tensor, List[torch.Tensor]]]) -> torch.Tensor: ...
+
+    @abstractmethod
+    def test_set_forward(self, teacher_index, batch) -> torch.Tensor:
         raise NotImplementedError("Base class method")
 
     @abstractmethod
-    def forward(self, teacher_index: int, batch: torch.Tensor) -> torch.Tensor:
+    def forward(self, teacher_index: int, batch: Dict[str, torch.Tensor]) -> torch.Tensor:
         """call to current teacher forward"""
         raise NotImplementedError("Base class method")
 
-    def forward_all(self, batch: torch.Tensor) -> List[torch.Tensor]:
+    def forward_all(self, batch: Dict) -> List[torch.Tensor]:
         """call to forward of all teachers (used primarily for evaluation)"""
         outputs = [self.forward(t, batch) for t in range(self._num_teachers)]
         return outputs
 
-    def get_teacher_networks(self) -> List[nn.Module]:
+    def get_teacher_networks(self) -> List[_Teacher]:
         """getter method for teacher networks"""
         networks = self._teachers 
         return networks
