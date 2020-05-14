396237c8e7b969961e6c472fb68c9a241776ae18
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-05-14 17:21:41 +0100

    removed now obsolete setup studnet method

--- models/learners/meta_learner.py
+++ models/learners/meta_learner.py
@@ -1,5 +1,3 @@
-from models.networks.students import MetaStudent
-
 from .base_learner import _BaseLearner
 
 from typing import Dict, List
@@ -17,12 +15,8 @@ class MetaLearner(_BaseLearner):
     def __init__(self, config):
         _BaseLearner.__init__(self, config)
 
-    def _setup_student(self, config: Dict) -> None:
-        """Instantiate student"""
-        self._student_network = MetaStudent(config=config).to(self._device)
-
     def signal_task_boundary_to_learner(self, new_task: int) -> None:
-        self._student_network.set_task(new_task)
+        self.set_task(new_task)
 
     def signal_step_boundary_to_learner(self, step: int, current_task: int) -> None:
         pass
