d93d4fe4f591e665b5f552fdae513310e7a6d725
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-05-17 15:18:09 +0100

    added linear function to package init for imports

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,8 @@
 from .parameters import Parameters
-from .custom_functions import visualise_matrix, get_pca
+from .custom_functions import visualise_matrix, get_pca, linear_function
 from .base_template import _Template, Field
 
 __all__ = [
-    "Parameters", "visualise_matrix", "get_pca", "_Template", "Field"
+    "Parameters", "visualise_matrix", "get_pca", "_Template", "Field",
+    "linear_function"
 ]
