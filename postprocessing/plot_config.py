5516ecd81f0b45ebff1d109607b61524c1803b23
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-05-20 09:31:04 +0100

    added color config, also added ability to save plots individually as wwell as combine to one

--- postprocessing/plot_config.py
+++ postprocessing/plot_config.py
@@ -1,4 +1,5 @@
 from utils import Parameters
+from constants import Constants
 from typing import Dict
 
 import itertools
@@ -15,26 +16,48 @@ class PlotConfigGenerator:
         teacher_hidden_layers = \
             config.get(["model", "teacher_hidden_layers"])
 
+        teacher_shades = Constants.TEACHER_SHADES
+        student_shades = Constants.STUDENT_SHADES
+        blue_shades = Constants.TORQUOISE_SHADES
+        green_shades = Constants.ORANGE_SHADES
+
+        shades = [blue_shades, green_shades]
+
         attribute_config = {}
 
-        generalisation_error_keys = [
+        generalisation_error_log_keys = [
             "teacher_{}_generalisation_error/log".format(t)
             for t in range(num_teachers)
         ]
         attribute_config["Generalisation Error (Log)"] = \
             {
+            "keys": generalisation_error_log_keys,
+            "plot_type": "scalar",
+            "labels": [
+                "Teacher {}".format(t) for t in range(num_teachers)
+                ],
+            "colours": teacher_shades
+            }
+
+        generalisation_error_keys = [
+            "teacher_{}_generalisation_error/linear".format(t)
+            for t in range(num_teachers)
+        ]
+        attribute_config["Generalisation Error"] = \
+            {
             "keys": generalisation_error_keys,
             "plot_type": "scalar",
             "labels": [
                 "Teacher {}".format(t) for t in range(num_teachers)
-                ]
+                ],
+            "colours": teacher_shades
             }
 
         for t in range(num_teachers):
 
             st_fill_ranges = [
-                range(len(student_hidden_layers[0])),
-                range(len(teacher_hidden_layers[0]))
+                range(student_hidden_layers[0]),
+                range(teacher_hidden_layers[0])
                 ]
             st_fill_combos = list(itertools.product(*st_fill_ranges))
 
@@ -43,26 +66,37 @@ class PlotConfigGenerator:
                 "layer_0_student_teacher_overlaps/{}/values_{}_{}".format(
                     t, i, j
                 )
-                for (i, j) in enumerate(st_fill_combos)
+                for (i, j) in st_fill_combos
             ]
             attribute_config["Student-Teacher {} Overlaps".format(t)] = \
                 {
                 "keys": student_teacher_t_overlaps,
                 "plot_type": "scalar",
-                "labels": []
+                "labels": [
+                    r"$R_{{{0}{1}}}$".format(i, j)
+                    for (i, j) in st_fill_combos
+                ],
+                "colours": shades[t][:len(st_fill_combos)]
                 }
             attribute_config[
                 "Student-Teacher {} Overlaps (Image)".format(t)
                 ] = {
-                    "keys": student_teacher_t_overlaps,
+                    "keys": {
+                        (j, k): student_teacher_t_overlaps[i]
+                        for i, (j, k) in enumerate(st_fill_combos)
+                        },
                     "plot_type": "image",
-                    "labels": []
+                    "key_format_ranges": [
+                        student_hidden_layers[0],
+                        teacher_hidden_layers[0]
+                        ],
+                    "labels": [],
                     }
 
             # student head weights
             student_head_weights = [
                 "student_head_{}_weight_{}".format(t, i)
-                for i in range(student_hidden_layers)
+                for i in range(student_hidden_layers[-1])
             ]
             attribute_config[
                 "Student Head Weights (Teacher {})".format(t)
@@ -71,13 +105,14 @@ class PlotConfigGenerator:
                     "plot_type": "scalar",
                     "labels": [
                         "Weight {}".format(h)
-                        for h in range(student_hidden_layers)
-                        ]
+                        for h in range(student_hidden_layers[-1])
+                        ],
+                    "colours": shades[t][:student_hidden_layers[-1]]
                     }
 
         ss_fill_ranges = [
-            range(len(student_hidden_layers[0])),
-            range(len(teacher_hidden_layers[0]))
+            range(student_hidden_layers[0]),
+            range(teacher_hidden_layers[0])
             ]
         ss_fill_combos = list(itertools.product(*ss_fill_ranges))
 
@@ -85,13 +120,17 @@ class PlotConfigGenerator:
             "layer_0_student_self_overlap/values_{}_{}".format(
                 i, j
             )
-            for (i, j) in enumerate(ss_fill_combos)
+            for (i, j) in ss_fill_combos
         ]
         attribute_config["Student-Student Overlaps"] = \
             {
             "keys": student_student_overlaps,
             "plot_type": "scalar",
-            "labels": []
+            "labels": [
+                r"$Q_{{{0}{1}}}$".format(i, j)
+                for (i, j) in ss_fill_combos
+            ],
+            "colours": student_shades[:len(ss_fill_combos)]
             }
 
         return attribute_config
