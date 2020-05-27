8f3268eb7fe8e5eca221dc0fe13c39687f0f69bd
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-05-27 13:22:37 +0100

    argparser class to init

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,9 @@
 from .parameters import Parameters
 from .custom_functions import visualise_matrix, get_pca, linear_function
 from .base_template import _Template, Field
+from .argparser import Argparser
 
 __all__ = [
     "Parameters", "visualise_matrix", "get_pca", "_Template", "Field",
-    "linear_function"
+    "linear_function", "Argparser"
 ]
