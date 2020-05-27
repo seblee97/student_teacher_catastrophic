8c1790d3a770233c15bdcdad3b45c8ffb226c2a7
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-05-27 14:00:04 +0100

    added cluster bash scripts

---
+++ experiments/cluster/submit_list_of_jobs.py
@@ -0,0 +1,31 @@
+import os
+import argparse
+from shutil import copyfile
+import subprocess
+
+argparser = argparse.ArgumentParser()
+
+# argparser.add_argument("-main_path", help="path to main file", type=str)
+argparser.add_argument("-list_path", help="path to file containing list of jobs", type=str)
+argparser.add_argument("-job_script_path", help="path to skeleton file for job instructions", type=str)
+argparser.add_argument("-num_cores", type=int, help="number of cores for job to use")
+
+args = argparser.parse_args()
+
+list_of_jobs = []
+
+with open(args.list_path) as file:
+    jobs = [j.rstrip() for j in file.readlines() if j.rstrip()]
+    for job in jobs:
+        list_of_jobs.append(job)
+
+current_file_path = os.path.dirname(os.path.realpath(__file__))
+
+for job in list_of_jobs:
+    job_file_copy = os.path.join(current_file_path, 'job_copy.sh')
+    print(job_file_copy)
+    copyfile(args.job_script_path, job_file_copy)
+    with open(job_file_copy, 'a') as file:
+        file.write("export OMP_NUM_THREADS={}\n".format(args.num_cores))
+        file.write(job)
+    subprocess.call("qsub -P saxe.prjc -q long.qc -pe shmem {} job_copy.sh".format(args.num_cores), shell=True)
