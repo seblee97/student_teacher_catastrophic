bdd86cae991b5d5f3d07c1734624594d566cdd14
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-05-28 14:52:20 +0100

    docs on args and 4th arg for extra flags for python cmd

--- experiments/process_runs.sh
+++ experiments/process_runs.sh
@@ -1,13 +1,19 @@
+# Args:
+# 1: whether or not to first scp folder from VM (0 = False, 1 = True)
+# 2: datestamp of experiment on VM
+# 3: folder name in cluster_results to which to save files
+# 4: extra arguments to provide post process call
+
 if [ $1 == "1" ]
 then
     scp -r rfl568@rescomp1.well.ox.ac.uk:/well/saxe/users/rfl568/repos/student_teacher_cont/experiments/results/$2 \
     /Users/sebastianlee/Dropbox/Documents/Research/Projects/catastrophic/experiments/results/cluster_results/$3
-else 
+else
     echo "scp completed as per first argument"
 fi
 
 for OUTPUT in $(ls /Users/sebastianlee/Dropbox/Documents/Research/Projects/catastrophic/experiments/results/cluster_results/$3)
 do
-    python main.py --ppp results/cluster_results/$3/$OUTPUT --ipf --nl
-    python main.py --ppp results/cluster_results/$3/$OUTPUT
+    python main.py --ppp results/cluster_results/$3/$OUTPUT --ipf --nl $4
+    python main.py --ppp results/cluster_results/$3/$OUTPUT $4
 done
