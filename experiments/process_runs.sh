1bda588648e20e07fbfb9496811fc58aa6b51751
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-05-30 11:54:56 +0100

    changed to rsync and made to exclude tb events files

--- experiments/process_runs.sh
+++ experiments/process_runs.sh
@@ -6,7 +6,7 @@
 
 if [ $1 == "1" ]
 then
-    scp -r rfl568@rescomp1.well.ox.ac.uk:/well/saxe/users/rfl568/repos/student_teacher_cont/experiments/results/$2 \
+    rsync -r --exclude='*events*' rfl568@rescomp1.well.ox.ac.uk:/well/saxe/users/rfl568/repos/student_teacher_cont/experiments/results/$2 \
     /Users/sebastianlee/Dropbox/Documents/Research/Projects/catastrophic/experiments/results/cluster_results/$3
 else
     echo "scp completed as per first argument"
