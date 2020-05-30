13f5e922ad1d15041f3b27f3fcd132acf60ee328
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-05-30 11:56:13 +0100

    added plot config for overlap differences

--- postprocessing/plot_config.py
+++ postprocessing/plot_config.py
@@ -3,6 +3,7 @@ from constants import Constants
 from typing import Dict, List
 
 import itertools
+import numpy as np
 
 
 class PlotConfigGenerator:
@@ -12,7 +13,8 @@ class PlotConfigGenerator:
     @staticmethod
     def generate_plotting_config(
         config: Parameters,
-        gradient_overlaps: bool
+        gradient_overlaps: bool,
+        overlap_differences: bool
     ) -> Dict[str, Dict]:
 
         num_teachers = config.get(["task", "num_teachers"])
@@ -50,10 +52,22 @@ class PlotConfigGenerator:
         if gradient_overlaps:
             attribute_config = {
                 **attribute_config,
-                **PlotConfigGenerator._get_student_grad_self_overlap_config(
+                **PlotConfigGenerator._get_student_grad_overlap_config(
+                    student_hidden_layers
+                    ),
+                **PlotConfigGenerator._get_log_student_grad_overlap_config(
                     student_hidden_layers
                     )
                 }
+        if overlap_differences:
+            for hidden_layer_index in range(len(student_hidden_layers)):
+                attribute_config = {
+                    **attribute_config,
+                    **PlotConfigGenerator._get_student_teacher_overlap_diff_config(
+                            hidden_layer_index, num_teachers,
+                            student_hidden_layers, teacher_hidden_layers
+                        )
+            }
 
         return attribute_config
 
@@ -151,6 +165,53 @@ class PlotConfigGenerator:
                     }
 
         return attribute_config
+    
+    @staticmethod
+    def _get_student_teacher_overlap_diff_config(
+        layer_index: int,
+        num_teachers: int,
+        student_hidden_layers: List[int],
+        teacher_hidden_layers: List[int]
+    ) -> Dict[str, Dict]:
+
+        attribute_config = {}
+
+        for t in range(num_teachers):
+
+            st_fill_ranges = [
+                range(student_hidden_layers[layer_index]),
+                range(teacher_hidden_layers[layer_index]),
+                range(student_hidden_layers[layer_index]),
+                range(teacher_hidden_layers[layer_index])
+                ]
+            st_fill_combos = list(itertools.product(*st_fill_ranges))
+
+            # first layer overlaps; student-teacher
+            student_teacher_t_overlaps = [
+                (
+                    "layer_{}_student_teacher_overlaps/{}/"
+                    "difference_{}_{}_vs_{}_{}".format(
+                        layer_index, t, i1, j1, i2, j2
+                    )
+                )
+                for (i1, j1, i2, j2) in st_fill_combos
+            ]
+            attribute_config[
+                "Student-Teacher {} Overlap Differences".format(t)
+                ] = \
+                {
+                "keys": student_teacher_t_overlaps,
+                "plot_type": "scalar",
+                "labels": [
+                    r"$R^{0}_{{{1}{2}}} - R^{0}_{{{3}{4}}}$".format(
+                        layer_index, i1, j1, i2, j2
+                        )
+                    for (i1, j1, i2, j2) in st_fill_combos
+                ],
+                "colours": PlotConfigGenerator.SHADES[t][:len(st_fill_combos)]
+                }
+
+        return attribute_config
 
     @staticmethod
     def _get_student_heads_config(
@@ -215,7 +276,7 @@ class PlotConfigGenerator:
         return attribute_config
 
     @staticmethod
-    def _get_student_grad_self_overlap_config(
+    def _get_student_grad_overlap_config(
         student_hidden_layers: List[int],
     ) -> Dict[str, Dict]:
 
@@ -243,6 +304,40 @@ class PlotConfigGenerator:
             ],
             "colours": Constants.STUDENT_SHADES[:len(ss_fill_combos)],
             "smoothing": 50
+            }
+
+        return attribute_config
+
+    @staticmethod
+    def _get_log_student_grad_overlap_config(
+        student_hidden_layers: List[int],
+    ) -> Dict[str, Dict]:
+
+        attribute_config = {}
+
+        ss_fill_ranges = [
+            range(student_hidden_layers[0]),
+            range(student_hidden_layers[0])
+            ]
+        ss_fill_combos = list(itertools.product(*ss_fill_ranges))
+
+        student_grad_student_overlaps = [
+            "layer_0_student_grad_student_overlap/values_{}_{}".format(
+                i, j
+            )
+            for (i, j) in ss_fill_combos
+        ]
+        attribute_config["Log Gradient-Student Overlaps"] = \
+            {
+            "keys": student_grad_student_overlaps,
+            "plot_type": "scalar",
+            "labels": [
+                r"$\nabla$" + r"$Q_{{{0}{1}}}$".format(i, j)
+                for (i, j) in ss_fill_combos
+            ],
+            "colours": Constants.STUDENT_SHADES[:len(ss_fill_combos)],
+            "smoothing": 50,
+            "transform_data": lambda x: np.log10(x)
             }
 
         return attribute_config
