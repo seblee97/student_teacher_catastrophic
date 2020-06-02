7c2a24825ffce4c6a18ffde803a442d4140e9b30
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-06-02 19:36:38 +0100

    new custom rfunctions in init

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,12 @@
 from .parameters import Parameters
-from .custom_functions import visualise_matrix, get_pca, linear_function
+from .custom_functions import visualise_matrix, get_pca, \
+    linear_function, close_sqrt_factors, get_figure_skeleton, \
+    smooth_data
 from .base_template import _Template, Field
 from .argparser import Argparser
 
 __all__ = [
     "Parameters", "visualise_matrix", "get_pca", "_Template", "Field",
-    "linear_function", "Argparser"
+    "linear_function", "Argparser", "close_sqrt_factors", "get_figure_skeleton",
+    "smooth_data"
 ]
