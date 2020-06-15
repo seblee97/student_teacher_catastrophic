2fd2aabdeb5836a78bb320cff44fadb32d2ed49f
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-06-15 10:31:38 +0100

    added iid data template to init

--- experiments/config_templates/__init__.py
+++ experiments/config_templates/__init__.py
@@ -1,9 +1,10 @@
 from .config_template import ConfigTemplate
+from .iid_data_template import IIDDataTemplate
 from .mnist_data_template import MNISTDataTemplate
 from .pure_mnist_template import PureMNISTTemplate
 from .trained_mnist_template import TrainedMNISTTemplate
 
 __all__ = [
     "ConfigTemplate", "MNISTDataTemplate", "PureMNISTTemplate",
-    "TrainedMNISTTemplate"
+    "TrainedMNISTTemplate", "IIDDataTemplate"
 ]
