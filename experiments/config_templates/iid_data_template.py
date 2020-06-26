81916cef84569827226e1e5597f0ed1901ed60ec
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-06-26 09:21:35 +0100

    added filed for dataset size in config template

--- experiments/config_templates/iid_data_template.py
+++ experiments/config_templates/iid_data_template.py
@@ -8,6 +8,11 @@ class IIDDataTemplate(_Template):
     LEVELS = ["iid_data"]
     OPTIONAL: List[str] = []
 
+    DATASET_SIZE = Field(
+        name="dataset_size", types=[int, str],
+        reqs=[lambda x: x == "inf" or (isinstance(x, int) and x > 0)]
+    )
+
     MEAN = Field(
         name="mean", types=[int, float], reqs=None
     )
@@ -19,6 +24,7 @@ class IIDDataTemplate(_Template):
     @classmethod
     def get_fields(cls) -> List:
         return [
+            cls.DATASET_SIZE,
             cls.MEAN,
             cls.VARIANCE
         ]
