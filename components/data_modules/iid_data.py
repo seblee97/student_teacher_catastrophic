29b4e582850d17b6ea03807911d216d4bef52a34
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-06-26 09:21:17 +0100

    changed iid into a proper torch dataloader if using finite data

--- components/data_modules/iid_data.py
+++ components/data_modules/iid_data.py
@@ -4,13 +4,38 @@ from constants import Constants
 
 import torch
 import torch.distributions as tdist
+from torch.utils.data import DataLoader
+import numpy as np
 
 from typing import Dict
 
 
+class IIDDataset(torch.utils.data.Dataset):
+
+    def __init__(
+        self,
+        dataset_size: int,
+        distribution: torch.distributions.Normal,
+        input_dimension
+    ) -> None:
+        self._dataset_size = dataset_size
+        self._data_distribution = distribution
+        self._input_dimension = input_dimension
+        
+        self._fixed_dataset = self._data_distribution.sample(
+            (self._dataset_size, self._input_dimension)
+            )
+
+    def __len__(self):
+        return self._dataset_size
+
+    def __getitem__(self, idx):
+        return self._fixed_dataset[idx]
+
+
 class IIDData(_BaseData):
     """
-    Class for generating data drawn i.i.d from unit normal 
+    Class for generating data drawn i.i.d from unit normal
     Gaussian.
     """
     def __init__(self, config: Parameters):
@@ -20,6 +45,16 @@ class IIDData(_BaseData):
         variance = config.get(["iid_data", "variance"])
 
         self._data_distribution = tdist.Normal(mean, variance)
+
+        self._dataset_size = config.get(["iid_data", "dataset_size"])
+
+        if self._dataset_size != "inf":
+            self._dataset = IIDDataset(
+                    dataset_size=self._dataset_size,
+                    distribution=self._data_distribution,
+                    input_dimension=self._input_dimension
+                    )
+            self._reset_data_iterator()
 
     def get_test_data(self) -> Constants.TEST_DATA_TYPES:
         """
@@ -38,10 +73,25 @@ class IIDData(_BaseData):
 
     def get_batch(self) -> Dict[str, torch.Tensor]:
         """returns batch of training data (input only)"""
-        batch = self._data_distribution.sample(
-            (self._train_batch_size, self._input_dimension)
-            ).to(self._device)
+        if self._dataset_size == "inf":
+            batch = self._data_distribution.sample(
+                        (self._train_batch_size, self._input_dimension)
+                    ).to(self._device)
+        else:
+            try:
+                batch = next(iter(self._data_iterator))
+            except StopIteration:
+                self._reset_data_iterator()
+                batch = next(iter(self._data_iterator))
         return {'x': batch}
+
+    def _reset_data_iterator(self):
+        self._dataloader = DataLoader(
+                self._dataset,
+                batch_size=self._train_batch_size,
+                shuffle=True
+                )
+        self._data_iterator = iter(self._dataloader)
 
     def signal_task_boundary_to_data_generator(self, new_task: int) -> None:
         pass
