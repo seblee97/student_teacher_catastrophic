42463c1ba76018b8d9db4dfbd82a0640986a7476
Sebastian Lee <sebastianlee@Sebastians-MacBook-Pro-2.local>
2020-07-27 19:05:26 +0100

    removed linear function

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,12 +1,10 @@
 from .parameters import Parameters
-from .custom_functions import visualise_matrix, get_pca, \
-    linear_function, close_sqrt_factors, get_figure_skeleton, \
+from .custom_functions import visualise_matrix, get_pca, close_sqrt_factors, get_figure_skeleton, \
     smooth_data
 from .base_template import _Template, Field
 from .argparser import Argparser
 
 __all__ = [
-    "Parameters", "visualise_matrix", "get_pca", "_Template", "Field",
-    "linear_function", "Argparser", "close_sqrt_factors", "get_figure_skeleton",
-    "smooth_data"
+    "Parameters", "visualise_matrix", "get_pca", "_Template", "Field", "linear_function",
+    "Argparser", "close_sqrt_factors", "get_figure_skeleton", "smooth_data"
 ]
