41bbc688537534cdc5cd4cd2b12cc5dcfcfe519c
Sebastian Lee <sebastianlee@Sebastians-MacBook-Pro-2.local>
2020-07-27 19:05:13 +0100

    formatting

--- ode/plotter.py
+++ ode/plotter.py
@@ -1,8 +1,10 @@
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-from mpl_toolkits.axes_grid1 import make_axes_locatable
+from typing import Dict
+from typing import List
+from typing import Tuple
+from typing import Union
 
-from typing import List, Dict, Union, Tuple
+import matplotlib.gridspec as gridspec
+import matplotlib.pyplot as plt
 
 
 class Plotter:
@@ -12,37 +14,20 @@ class Plotter:
         self._log_keys = list(self._logs.keys())
 
     @staticmethod
-    def get_figure_skeleton(
-        height: Union[int, float],
-        width: Union[int, float],
-        num_columns: int,
-        num_rows: int
-    ) -> Tuple:
+    def get_figure_skeleton(height: Union[int, float], width: Union[int, float], num_columns: int,
+                            num_rows: int) -> Tuple:
 
-        fig = plt.figure(
-            constrained_layout=False,
-            figsize=(
-                num_columns * width,
-                num_rows * height
-                )
-            )
+        fig = plt.figure(constrained_layout=False, figsize=(num_columns * width, num_rows * height))
 
         heights = [height for _ in range(num_rows)]
         widths = [width for _ in range(num_columns)]
 
         spec = gridspec.GridSpec(
-            nrows=num_rows, ncols=num_columns,
-            width_ratios=widths, height_ratios=heights
-            )
+            nrows=num_rows, ncols=num_columns, width_ratios=widths, height_ratios=heights)
 
         return fig, spec
 
-    def _plot_scalar(
-        self, 
-        row: int,
-        col: int,
-        graph_index: int
-        ):
+    def _plot_scalar(self, row: int, col: int, graph_index: int):
         log_key = self._log_keys[graph_index]
         logs = self._logs[log_key]
 
@@ -50,11 +35,7 @@ class Plotter:
 
         for data_index, data in logs.items():
 
-            fig_sub.plot(
-                range(len(data)),
-                data,
-                label=f"{log_key}_{data_index}"
-                )
+            fig_sub.plot(range(len(data)), data, label=f"{log_key}_{data_index}")
 
         # labelling
         fig_sub.set_xlabel("Step")
@@ -63,18 +44,12 @@ class Plotter:
 
         # grids
         fig_sub.minorticks_on()
-        fig_sub.grid(
-            which='major', linestyle='-', linewidth='0.5',
-            color='red', alpha=0.2
-            )
-        fig_sub.grid(
-            which='minor', linestyle=':', linewidth='0.5',
-            color='black', alpha=0.4
-            )
-        
+        fig_sub.grid(which='major', linestyle='-', linewidth='0.5', color='red', alpha=0.2)
+        fig_sub.grid(which='minor', linestyle=':', linewidth='0.5', color='black', alpha=0.4)
+
     def plot(self):
-        
-        # 1. Errors (Linear) 2. Errors (Log) 
+
+        # 1. Errors (Linear) 2. Errors (Log)
         # 3. Q, 4. R, 5. U 6. h1, 7. h2
 
         graph_layout = (5, 2)
@@ -83,9 +58,7 @@ class Plotter:
         num_columns = graph_layout[1]
 
         self.fig, self.spec = self.get_figure_skeleton(
-            height=4, width=5, num_columns=num_columns,
-            num_rows=num_rows
-            )
+            height=4, width=5, num_columns=num_columns, num_rows=num_rows)
 
         for row in range(num_rows):
             for col in range(num_columns):
@@ -94,16 +67,8 @@ class Plotter:
 
                 if graph_index < num_graphs:
 
-                    print(
-                        "Plotting graph {}/{}".format(
-                            graph_index + 1, 10
-                            )
-                        )
+                    print("Plotting graph {}/{}".format(graph_index + 1, 10))
 
-                    self._plot_scalar(
-                        row=row,
-                        col=col,
-                        graph_index=graph_index
-                        )
+                    self._plot_scalar(row=row, col=col, graph_index=graph_index)
 
         return self.fig
