7db200e56f019e23e2c300d53f0a2382ed5ccba0
Sebastian Lee <sebastianlee@Sebastians-MacBook-Pro-2.local>
2020-07-27 19:04:32 +0100

    curriculum now try to get next else inf i.e no more switching

--- ode/dynamics.py
+++ ode/dynamics.py
@@ -1,33 +1,33 @@
-import numpy as np
-import itertools
 import os
+import warnings
+from typing import List
+from typing import Optional
+
+import numpy as np
 
 from ode.configuration import StudentTwoTeacherConfiguration
 from ode.integrals import Integrals
 from ode.plotter import Plotter
 
-from typing import List, Tuple, Dict, Optional
-
 
 class StudentTeacherODE:
 
-    def __init__(
-        self, 
-        overlap_configuration: StudentTwoTeacherConfiguration, 
-        nonlinearity: str, 
-        w_learning_rate: float, 
-        h_learning_rate: float,
-        curriculum: List[int]
-    ):
+    def __init__(self, overlap_configuration: StudentTwoTeacherConfiguration, nonlinearity: str,
+                 w_learning_rate: float, h_learning_rate: float, curriculum: List[int],
+                 soft_committee: bool):
 
         self._configuration = overlap_configuration
         self._nonlinearity = nonlinearity
         self._w_learning_rate = w_learning_rate
         self._h_learning_rate = h_learning_rate
+        self._soft_committee = soft_committee
 
         if curriculum is not None:
-            self._curriculum = iter(curriculum[1:])
-            self._next_switch_step = next(self._curriculum)
+            self._curriculum = iter(curriculum)
+            try:
+                self._next_switch_step = next(self._curriculum)
+            except StopIteration:
+                self._next_switch_step = np.inf
         else:
             self._curriculum = None
 
@@ -53,7 +53,7 @@ class StudentTeacherODE:
     @property
     def timestep(self):
         return self._timestep
-    
+
     @property
     def error_1_log(self):
         return self._error_1_log
@@ -76,7 +76,8 @@ class StudentTeacherODE:
         for (i, n), _ in np.ndenumerate(derivative):
             in_derivative = 0
             for m, head_unit in enumerate(teacher_head):
-                cov = self._configuration.generate_covariance_matrix([("i", i), ("n", n), (teacher_index, m)])
+                cov = self._configuration.generate_covariance_matrix([("i", i), ("n", n),
+                                                                      (teacher_index, m)])
                 in_derivative += head_unit * self._i3_fn(cov)
             for k, head_unit in enumerate(student_head):
                 cov = self._configuration.generate_covariance_matrix([("i", i), ("n", n), ("k", k)])
@@ -104,7 +105,8 @@ class StudentTeacherODE:
 
             sum_1 = 0
             for m, head_unit in enumerate(teacher_head):
-                cov = self._configuration.generate_covariance_matrix([("i", i), ("k", k), (teacher_index, m)])
+                cov = self._configuration.generate_covariance_matrix([("i", i), ("k", k),
+                                                                      (teacher_index, m)])
                 sum_1 += head_unit * self._i3_fn(cov)
             for j, head_unit in enumerate(student_head):
                 cov = self._configuration.generate_covariance_matrix([("i", i), ("k", k), ("j", j)])
@@ -114,29 +116,35 @@ class StudentTeacherODE:
 
             sum_2 = 0
             for m, head_unit in enumerate(teacher_head):
-                cov = self._configuration.generate_covariance_matrix([("k", k), ("i", i), (teacher_index, m)])
+                cov = self._configuration.generate_covariance_matrix([("k", k), ("i", i),
+                                                                      (teacher_index, m)])
                 sum_2 += head_unit * self._i3_fn(cov)
             for j, head_unit in enumerate(student_head):
                 cov = self._configuration.generate_covariance_matrix([("k", k), ("i", i), ("j", j)])
                 sum_2 -= head_unit * self._i3_fn(cov)
-            
+
             ik_derivative += self._w_learning_rate * student_head[k] * sum_2
 
             sum_3 = 0
             for j, head_unit_j in enumerate(student_head):
                 for l, head_unit_l in enumerate(student_head):
-                    cov = self._configuration.generate_covariance_matrix([("i", i), ("k", k), ("j", j), ("l", l)])
+                    cov = self._configuration.generate_covariance_matrix([("i", i), ("k", k),
+                                                                          ("j", j), ("l", l)])
                     sum_3 += head_unit_j * head_unit_l * self._i4_fn(cov)
             for m, head_unit_m in enumerate(teacher_head):
                 for n, head_unit_n in enumerate(teacher_head):
-                    cov = self._configuration.generate_covariance_matrix([("i", i), ("k", k), (teacher_index, m), (teacher_index_2, n)])
+                    cov = self._configuration.generate_covariance_matrix([("i", i), ("k", k),
+                                                                          (teacher_index, m),
+                                                                          (teacher_index_2, n)])
                     sum_3 += head_unit_m * head_unit_n * self._i4_fn(cov)
             for m, head_unit_m in enumerate(teacher_head):
                 for j, head_unit_j in enumerate(student_head):
-                    cov = self._configuration.generate_covariance_matrix([("i", i), ("k", k), ("j", j), (teacher_index, m)])
+                    cov = self._configuration.generate_covariance_matrix([("i", i), ("k", k),
+                                                                          ("j", j),
+                                                                          (teacher_index, m)])
                     sum_3 -= 2 * head_unit_m * head_unit_j * self._i4_fn(cov)
 
-            ik_derivative += self._w_learning_rate ** 2 * student_head[i] * student_head[k] * sum_3
+            ik_derivative += self._w_learning_rate**2 * student_head[i] * student_head[k] * sum_3
 
             derivative[i][k] = ik_derivative
 
@@ -156,7 +164,8 @@ class StudentTeacherODE:
         for (i, p), _ in np.ndenumerate(derivative):
             ip_derivative = 0
             for m, head_unit in enumerate(teacher_head):
-                cov = self._configuration.generate_covariance_matrix([("i", i), ("p", p), (teacher_index, m)])
+                cov = self._configuration.generate_covariance_matrix([("i", i), ("p", p),
+                                                                      (teacher_index, m)])
                 ip_derivative += head_unit * self._i3_fn(cov)
             for k, head_unit in enumerate(student_head):
                 cov = self._configuration.generate_covariance_matrix([("i", i), ("p", p), ("k", k)])
@@ -169,7 +178,7 @@ class StudentTeacherODE:
     @property
     def dh1_dt(self):
         derivative = np.zeros(self._configuration.h1.shape).astype(float)
-        if self._active_teacher == 0:
+        if not self._soft_committee and self._active_teacher == 0:
             for i in range(len(derivative)):
                 i_derivative = 0
                 for m, head_unit in enumerate(self._configuration.th1):
@@ -178,7 +187,7 @@ class StudentTeacherODE:
                 for k, head_unit in enumerate(self._configuration.h1):
                     cov = self._configuration.generate_covariance_matrix([("i", i), ("k", k)])
                     i_derivative -= head_unit * self._i2_fn(cov)
-                
+
                 derivative[i] = self._h_learning_rate * i_derivative
 
         return derivative
@@ -186,7 +195,7 @@ class StudentTeacherODE:
     @property
     def dh2_dt(self):
         derivative = np.zeros(self._configuration.h2.shape).astype(float)
-        if self._active_teacher == 1:
+        if not self._soft_committee and self._active_teacher == 1:
             for i in range(len(derivative)):
                 i_derivative = 0
                 for p, head_unit in enumerate(self._configuration.th2):
@@ -195,7 +204,7 @@ class StudentTeacherODE:
                 for k, head_unit in enumerate(self._configuration.h2):
                     cov = self._configuration.generate_covariance_matrix([("i", i), ("k", k)])
                     i_derivative -= head_unit * self._i2_fn(cov)
-                
+
                 derivative[i] = self._h_learning_rate * i_derivative
 
         return derivative
@@ -207,7 +216,7 @@ class StudentTeacherODE:
     @property
     def de_passive_dt(self):
         raise NotImplementedError
-    
+
     @property
     def error_1(self):
         error = 0
@@ -224,9 +233,19 @@ class StudentTeacherODE:
                 cov = self._configuration.generate_covariance_matrix([("i", i), ("n", n)])
                 error -= 2 * head_unit_i * teacher_head_unit_n * self._i2_fn(cov)
         if error < 0:
-            import pdb; pdb.set_trace()
+            warnings.warn(
+                "Latest error calculation is negative. This could be due to the learning rate being too high, "
+                "especially close to convergence. Run 'self.error_1_log' or self.error_2_log' to view error logs to this point."
+                "Run self.make_plot(SAVE_PATH) to create plot of errors and overlaps to this point."
+            )
+            import pdb
+            pdb.set_trace()
         if np.isnan(error):
-            import pdb; pdb.set_trace()
+            warnings.warn(
+                "Latest error calculation is NaN. "
+                "Run 'self.error_1_log' or self.error_2_log' to view error logs to this point.")
+            import pdb
+            pdb.set_trace()
         return error
 
     @property
@@ -267,18 +286,18 @@ class StudentTeacherODE:
         self._error_1_log.append(error_1)
         self._error_2_log.append(error_2)
 
-    def _switch_teacher(self, num_steps: int):
+    def _switch_teacher(self):
         self._active_teacher = int(not self._active_teacher)
         try:
             self._next_switch_step = next(self._curriculum)
-        except:
-            self._next_switch_step = num_steps
+        except StopIteration:
+            self._next_switch_step = np.inf
 
     def step(self, num_steps: int):
         for i in range(num_steps):
             if self._curriculum is not None:
                 if i == self._next_switch_step:
-                    self._switch_teacher(num_steps)
+                    self._switch_teacher()
             if i % 1000 == 0:
                 print(f"Step {i} of ODE dynamics")
             self._step()
@@ -303,13 +322,7 @@ class StudentTeacherODE:
             "h2": self._configuration.h2_log
         }
 
-        fig = Plotter({
-            **error_dict,
-            **overlap_dict
-        }).plot()
+        fig = Plotter({**error_dict, **overlap_dict}).plot()
 
         if save_path:
-            fig.savefig(
-                os.path.join(save_path, "ode_plot_summary.pdf"), 
-                dpi=100
-                )
+            fig.savefig(os.path.join(save_path, "ode_plot_summary.pdf"), dpi=100)
