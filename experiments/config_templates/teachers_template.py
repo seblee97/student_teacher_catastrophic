bfad05094ca2c35439e28dd7bdce3fdeff18b603
Sebastian Lee <sebastianlee@Sebastians-MacBook-Pro-2.local>
2020-07-31 18:25:50 +0100

    teacher overlaps now also implemented as rotation i,e. true overlaps and not just weight copies2C

--- experiments/config_templates/teachers_template.py
+++ experiments/config_templates/teachers_template.py
@@ -1,6 +1,9 @@
-from utils import _Template, Field
-
 from typing import List
+
+import numpy as np
+
+from utils import Field
+from utils import _Template
 
 
 class TeachersTemplate(_Template):
@@ -10,18 +13,26 @@ class TeachersTemplate(_Template):
 
     # Teachers level fields
     OVERLAP_PERCENTAGES = Field(
-        name="overlap_percentages", types=[list],
-        reqs=[lambda x: all(isinstance(y, int) and y >= 0 for y in x)]
-    )
+        name="overlap_percentages",
+        types=[list],
+        reqs=[lambda x: all(isinstance(y, int) and y >= 0 for y in x)])
+
+    OVERLAP_ROTATIONS = Field(
+        name="overlap_rotations",
+        types=[list],
+        reqs=[
+            lambda x: all((isinstance(y, float) or isinstance(y, int)) and y >= 0 and y <= 2 * np.pi
+                          for y in x)
+        ])
 
     TEACHER_NOISE = Field(
-        name="teacher_noise", types=[list],
-        reqs=[lambda x: all(isinstance(y, (float, int)) and y >= 0 for y in x)]
-    )
+        name="teacher_noise",
+        types=[list],
+        reqs=[lambda x: all(isinstance(y, (float, int)) and y >= 0 for y in x)])
+
+    OVERLAP_TYPE = Field(
+        name="overlap_type", types=[str], reqs=[lambda x: x in ["rotation", "copy"]])
 
     @classmethod
     def get_fields(cls) -> List:
-        return [
-            cls.OVERLAP_PERCENTAGES,
-            cls.TEACHER_NOISE
-        ]
+        return [cls.OVERLAP_PERCENTAGES, cls.OVERLAP_ROTATIONS, cls.TEACHER_NOISE, cls.OVERLAP_TYPE]
