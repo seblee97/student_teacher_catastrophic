98fee4dfaf7ecdf519f00edacef055c0511c1967
Sebastian Lee <sebastianlee@Sebastians-MacBook-Pro-2.local>
2020-08-05 13:22:44 +0100

    overlay plotter to init

--- post_processing/__init__.py
+++ post_processing/__init__.py
@@ -1,3 +1,4 @@
 from .student_teacher_post_processing import StudentTeacherPostProcessor
+from .overlay_plotter import OverlayPlotter
 
-__all__ = ["StudentTeacherPostProcessor"]
+__all__ = ["StudentTeacherPostProcessor", "OverlayPlotter"]
