e487d8431297c9a3a3a61cbf611cda529b55b4b2
Sebastian Lee <sebastianlee@Sebastians-MacBook-Pro-2.local>
2020-08-18 20:41:38 +0100

    changed to start with overlaps

--- jobs.py
+++ jobs.py
@@ -1,8 +1,9 @@
 import subprocess
 import numpy as np
 
-teacher_teacher_overlaps = np.linspace(0, np.pi, 50)
+teacher_teacher_overlaps = np.linspace(0, 1, 50)
+rotations = np.arccos(teacher_teacher_overlaps)
 
-for o, overlap in enumerate(teacher_teacher_overlaps):
+for o, rotation in enumerate(rotations):
     subprocess.Popen(
-        f"python experiments/main.py --tro '[{round(overlap, 4)}, 0]' --en {o}", shell=True)
+        f"python experiments/main.py --tro '[{round(rotation, 4)}, 0]' --en {o}", shell=True)
