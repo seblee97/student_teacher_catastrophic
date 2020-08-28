5098c48fd48b1fc032becfd19d61545085ae308e
Sebastian Lee <sebastianlee@Sebastians-MacBook-Pro-2.local>
2020-08-28 21:57:24 +0100

    log overlaps [arameter added

--- experiments/config_templates/logging_template.py
+++ experiments/config_templates/logging_template.py
@@ -9,43 +9,26 @@ class LoggingTemplate(_Template):
     OPTIONAL: List[str] = []
 
     # Logging level fields
-    VERBOSE = Field(
-        name="verbose", types=[bool], reqs=None
-    )
+    VERBOSE = Field(name="verbose", types=[bool], reqs=None)
 
-    VERBOSE_TB = Field(
-        name="verbose_tb", types=[int],
-        reqs=[lambda x: x in [0, 1, 2]]
-    )
+    VERBOSE_TB = Field(name="verbose_tb", types=[int], reqs=[lambda x: x in [0, 1, 2]])
 
-    CHECKPOINT_FREQUENCY = Field(
-        name="checkpoint_frequency", types=[int], reqs=None
-    )
+    CHECKPOINT_FREQUENCY = Field(name="checkpoint_frequency", types=[int], reqs=None)
 
-    LOG_TO_DF = Field(
-        name="log_to_df", types=[bool], reqs=None
-    )
+    LOG_TO_DF = Field(name="log_to_df", types=[bool], reqs=None)
 
-    MERGE_AT_CHECKPOINT = Field(
-        name="merge_at_checkpoint", types=[bool], reqs=None
-    )
+    MERGE_AT_CHECKPOINT = Field(name="merge_at_checkpoint", types=[bool], reqs=None)
 
-    SAVE_WEIGHTS_AT_SWITCH = Field(
-        name="save_weights_at_switch", types=[bool], reqs=None
-    )
+    SAVE_WEIGHTS_AT_SWITCH = Field(name="save_weights_at_switch", types=[bool], reqs=None)
 
-    SAVE_INITIAL_WEIGHTS = Field(
-        name="save_initial_weights", types=[bool], reqs=None
-    )
+    SAVE_INITIAL_WEIGHTS = Field(name="save_initial_weights", types=[bool], reqs=None)
+
+    LOG_OVERLAPS = Field(name="log_overlaps", types=[bool], reqs=None)
 
     @classmethod
     def get_fields(cls) -> List:
         return [
-            cls.VERBOSE,
-            cls.VERBOSE_TB,
-            cls.CHECKPOINT_FREQUENCY,
-            cls.LOG_TO_DF,
-            cls.MERGE_AT_CHECKPOINT,
-            cls.SAVE_WEIGHTS_AT_SWITCH,
-            cls.SAVE_INITIAL_WEIGHTS
+            cls.VERBOSE, cls.VERBOSE_TB, cls.CHECKPOINT_FREQUENCY, cls.LOG_TO_DF,
+            cls.MERGE_AT_CHECKPOINT, cls.SAVE_WEIGHTS_AT_SWITCH, cls.SAVE_INITIAL_WEIGHTS,
+            cls.LOG_OVERLAPS
         ]
