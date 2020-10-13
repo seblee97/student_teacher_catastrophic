3af2c44e152722a72109a5859cff4e08744d4770
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-10-13 12:00:27 +0100

    removed type constants

--- constants.py
+++ constants.py
@@ -1,15 +1,4 @@
-from typing import Union, List, Dict, Iterator
-
-import torch
-from torchvision.datasets.mnist import MNIST
-
-
 class Constants:
-
-    TEST_DATA_TYPES = Union[
-        List[Dict[str, torch.Tensor]],
-        Dict[str, Union[torch.Tensor, List[torch.Tensor]]]
-        ]
 
     EVEN_ODD_MAPPING = {
         0: 0, 1: 1, 2: 0, 3: 1, 4: 0, 5: 1, 6: 0, 7: 1, 8: 0, 9: 1
@@ -18,15 +7,6 @@ class Constants:
     GREATER_FIVE_MAPPING = {
         0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 1, 6: 1, 7: 1, 8: 1, 9: 1
         }
-
-    TRAINABLE_PARAMETERS_TYPE = Union[
-        List[Dict[str, Iterator[torch.nn.Parameter]]],
-        Iterator[torch.nn.Parameter]
-        ]
-
-    MNIST_DATASET_TYPE = MNIST
-
-    DATASET_TYPES = Union[List[MNIST], MNIST]
 
     # Hard-coded subplot layouts for different numbers of graphs
     GRAPH_LAYOUTS = {
