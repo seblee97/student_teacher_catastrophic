83ffcc23059dd98e56e3e0c3cbc093cb753ad19f
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-10-26 15:01:21 +0000

    added next method and abstract method for switch

--- curricula/base_curriculum.py
+++ curricula/base_curriculum.py
@@ -1,13 +1,31 @@
 import abc
+import itertools
+
+from run import student_teacher_config
 
 
 class BaseCurriculum(abc.ABC):
     """Base class for curriculum objects."""
 
-    def __init__(self):
-        pass
+    def __init__(
+        self, config: student_teacher_config.StudentTeacherConfiguration
+    ) -> None:
+        self._curriculum = itertools.cycle(list(range(config.num_teachers)))
+
+    def __next__(self):
+        """Get next index from curriculum."""
+        return next(self._curriculum)
 
     @abc.abstractmethod
-    def to_switch(self, step: int, error: float):
-        """Establish whether condition for switching has been met."""
+    def to_switch(self, task_step: int, error: float) -> bool:
+        """Establish whether condition for switching has been met.
+
+        Args:
+            task_step: number of steps completed for current task
+            being trained (not overall step count).
+            error: generalisation error associated with current teacher.
+
+        Returns:
+            bool indicating whether or not to switch.
+        """
         pass
