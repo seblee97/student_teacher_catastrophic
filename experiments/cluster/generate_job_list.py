0b0f7ed67c10ee7a4a91f3db38aff7ca18fd62a4
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-11-04 13:39:10 +0000

    removed defunct experiments module

--- experiments/cluster/generate_job_list.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-with open("job_lists/relu_vs_v.txt", "w") as f:
-    teacher_teacher_overlaps = np.linspace(0, 1, 50)
-    rotations = np.arccos(teacher_teacher_overlaps)
-    for o, rotation in enumerate(rotations):
-        f.write(f"python main.py --en {o} --tro '[0, {round(rotation, 4)}]'\n")
