db68dff11b6ed361ff9b1a28a37f97b52240887a
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-11-22 16:25:24 +0000

    split summary plot methods into gen errors, cross sections,rates, made rate method

--- run/multi_run_pipeline.py
+++ run/multi_run_pipeline.py
@@ -178,7 +178,7 @@ def single_run(
 
 def get_dfs(folder: str, seeds: List[int]) -> Dict[str, List[pd.DataFrame]]:
     dfs = {}
-    indices = os.listdir(folder)
+    indices = [index for index in os.listdir(folder) if not index.startswith(".")]
     for index in indices:
         index_dfs = []
         for seed in seeds:
@@ -187,20 +187,10 @@ def get_dfs(folder: str, seeds: List[int]) -> Dict[str, List[pd.DataFrame]]:
     return dfs
 
 
-def summary_plot(
-    config: student_teacher_config.StudentTeacherConfiguration,
-    experiment_path: str,
-    seeds: List[int],
-):
-    num_steps = config.total_training_steps
-    num_ode_steps = (
-        config.total_training_steps / config.input_dimension / config.timestep
-    )
-
-    # plot generalisation error over time for each overlap
-    dfs = get_dfs(folder=experiment_path, seeds=seeds)
-    indices = sorted(dfs.keys(), key=lambda x: float(x.split("_")[1]))
-
+def generalisation_error_figs(
+    dfs: List[pd.DataFrame], indices: List[str], num_steps: int
+) -> None:
+    """Plot generalisation error over time for each overlap."""
     teacher_1_fig = plt.figure()
     teacher_1_colormap = cm.get_cmap(constants.Constants.VIRIDIS)
 
@@ -234,30 +224,36 @@ def summary_plot(
     teacher_2_fig.savefig(save_name, dpi=100)
     plt.close()
 
-    # plot forgetting/transfer vs. v for various time intervals post switch
-    # scale step for ODE time
-    switch_step = int(config.switch_steps[0] / config.input_dimension / config.timestep)
 
-    for interval in np.logspace(np.log10(switch_step), np.log10(num_ode_steps), 10):
+def cross_section_figs(
+    dfs: List[pd.DataFrame],
+    indices: List[str],
+    switch_step: int,
+    num_ode_steps: int,
+):
+    """Plot forgetting/transfer vs. v for various time intervals post switch."""
+    overlaps = [float(index.split("_")[1]) for index in indices]
+
+    for interval in np.linspace(switch_step, num_ode_steps, 10):
 
         error_deltas_0 = []
         error_deltas_1 = []
         for i, index in enumerate(indices):
-            log_generalisation_error_0 = dfs[index][0][
-                f"{constants.Constants.LOG_GENERALISATION_ERROR}_0"
+            generalisation_error_0 = dfs[index][0][
+                f"{constants.Constants.GENERALISATION_ERROR}_0"
             ].to_numpy()
-            log_generalisation_error_1 = dfs[index][0][
-                f"{constants.Constants.LOG_GENERALISATION_ERROR}_1"
+            generalisation_error_1 = dfs[index][0][
+                f"{constants.Constants.GENERALISATION_ERROR}_1"
             ].to_numpy()
-            switch_error_0 = log_generalisation_error_0[switch_step]
-            switch_error_1 = log_generalisation_error_1[switch_step]
-            error_delta_0 = log_generalisation_error_0[int(interval)] - switch_error_0
-            error_delta_1 = log_generalisation_error_1[int(interval)] - switch_error_1
+            switch_error_0 = generalisation_error_0[switch_step]
+            switch_error_1 = generalisation_error_1[switch_step]
+            error_delta_0 = generalisation_error_0[int(interval)] - switch_error_0
+            error_delta_1 = switch_error_1 - generalisation_error_1[int(interval)]
             error_deltas_0.append(error_delta_0)
             error_deltas_1.append(error_delta_1)
 
         forgetting_vs_v_fig = plt.figure()
-        plt.plot(indices, error_deltas_0, linewidth=5)
+        plt.plot(overlaps, error_deltas_0, linewidth=5)
         save_name = os.path.join(
             experiment_path,
             f"{int(interval) - switch_step}_{constants.Constants.FORGETTING_VS_V_PLOT}",
@@ -266,13 +262,87 @@ def summary_plot(
         plt.close()
 
         transfer_vs_v_fig = plt.figure()
-        plt.plot(indices, error_deltas_1, linewidth=5)
+        plt.plot(overlaps, error_deltas_1, linewidth=5)
         save_name = os.path.join(
             experiment_path,
             f"{int(interval) - switch_step}_{constants.Constants.TRANSFER_VS_V_PLOT}",
         )
         transfer_vs_v_fig.savefig(save_name, dpi=100)
         plt.close()
+
+
+def rate_figs(dfs: List[pd.DataFrame], indices: List[str], switch_step: int):
+    """Plot initial forgetting/transfer rate vs. v."""
+
+    forgetting_rates = []
+    transfer_rates = []
+
+    overlaps = [float(index.split("_")[1]) for index in indices]
+
+    for i, index in enumerate(indices):
+        generalisation_error_0 = dfs[index][0][
+            f"{constants.Constants.GENERALISATION_ERROR}_0"
+        ].to_numpy()
+        generalisation_error_1 = dfs[index][0][
+            f"{constants.Constants.GENERALISATION_ERROR}_1"
+        ].to_numpy()
+
+        initial_error_deltas_0 = [
+            generalisation_error_0[switch_step + i + 1]
+            - generalisation_error_0[switch_step + i]
+            for i in range(10)
+        ]
+        initial_error_deltas_1 = [
+            generalisation_error_1[switch_step + i]
+            - generalisation_error_1[switch_step + i + 1]
+            for i in range(10)
+        ]
+
+        error_delta_rate_0 = np.mean(initial_error_deltas_0)
+        error_delta_rate_1 = np.mean(initial_error_deltas_1)
+
+        forgetting_rates.append(error_delta_rate_0)
+        transfer_rates.append(error_delta_rate_1)
+
+    forgetting_rate_fig = plt.figure()
+    plt.plot(overlaps, forgetting_rates, linewidth=5)
+    save_name = os.path.join(
+        experiment_path,
+        constants.Constants.FORGETTING_RATE_PLOT,
+    )
+    forgetting_rate_fig.savefig(save_name, dpi=100)
+    plt.close()
+
+    transfer_rate_fig = plt.figure()
+    plt.plot(overlaps, transfer_rates, linewidth=5)
+    save_name = os.path.join(
+        experiment_path,
+        constants.Constants.TRANSFER_RATE_PLOT,
+    )
+    transfer_rate_fig.savefig(save_name, dpi=100)
+    plt.close()
+
+
+def summary_plot(
+    config: student_teacher_config.StudentTeacherConfiguration,
+    experiment_path: str,
+    seeds: List[int],
+):
+    num_steps = config.total_training_steps
+    num_ode_steps = (
+        config.total_training_steps / config.input_dimension / config.timestep
+    )
+    # scale step for ODE time
+    switch_step = int(config.switch_steps[0] / config.input_dimension / config.timestep)
+
+    dfs = get_dfs(folder=experiment_path, seeds=seeds)
+    indices = sorted(dfs.keys(), key=lambda x: float(x.split("_")[1]))
+
+    generalisation_error_figs(dfs=dfs, indices=indices, num_steps=num_steps)
+    cross_section_figs(
+        dfs=dfs, indices=indices, switch_step=switch_step, num_ode_steps=num_ode_steps
+    )
+    rate_figs(dfs=dfs, indices=indices, switch_step=switch_step)
 
 
 if __name__ == "__main__":
