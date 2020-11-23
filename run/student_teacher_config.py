26d76580d0e1bbb67782fcbbabab4c2ff02189aa
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-11-23 13:06:24 +0000

    added explicit check for scaling of forward by hidden dimension if using the both

--- run/student_teacher_config.py
+++ run/student_teacher_config.py
@@ -3,6 +3,8 @@ from typing import Union, Dict
 from config_manager import base_configuration
 from config_manager import config_template
 
+import constants
+
 
 class StudentTeacherConfiguration(base_configuration.BaseConfiguration):
     def __init__(self, config: Union[str, Dict], template: config_template.Template):
@@ -13,4 +15,13 @@ class StudentTeacherConfiguration(base_configuration.BaseConfiguration):
         """Method to check for non-trivial associations
         in the configuration.
         """
-        pass
+        if self.teacher_configuration == constants.Constants.BOTH_ROTATION:
+            assert self.scale_forward_by_hidden == True, (
+                "In both rotation regime, i.e. mean field limit, "
+                "need to scale forward by 1/K."
+            )
+        else:
+            assert self.scale_forward_by_hidden == False, (
+                "When not in both rotation regime, i.e. mean field limit, "
+                "no need to scale forward by 1/K."
+            )
