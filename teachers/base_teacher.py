e49043df63ff1c3d8b67f63ea776a3d8fc6ab7f6
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-12-11 12:39:15 +0000

    removed hashed code and generalised unit norm teacher head

--- teachers/base_teacher.py
+++ teachers/base_teacher.py
@@ -55,39 +55,16 @@ class BaseTeacher(base_network.BaseNetwork, abc.ABC):
             for param in layer.parameters():
                 param.requires_grad = False
 
-    # def get_output_statistics(self, repeats=5000):
-    #     with torch.no_grad():
-    #         random_input = torch.randn(repeats, self.input_dimension)
-    #         output = self.forward(random_input)
-    #         output_std = float(torch.std(output))
-    #     return output_std
-
-    # def add_output_noise(self, clean_tensor: torch.Tensor) -> torch.Tensor:
-    #     noise_sample = self.noise_distribution.sample(clean_tensor.shape)
-    #     return clean_tensor + noise_sample
-
-    # def set_noise_distribution(self, mean: float, std: float):
-    #     """
-    #     Sets a normal distribution from which to sample noise that is
-    #     added to output
-    #     """
-    #     if std == 0:
-    #         raise ValueError("Standard Deviation of Normal cannot be 0.")
-    #     self.noise_distribution = tdist.Normal(mean, std)
-    #     self.noisy = True
-
-    # def load_weights(self, weights_path: str) -> None:
-    #     """Load saved weights into model"""
-    #     torch.load(weights_path)
-    #     self.load_state_dict(torch.load(weights_path))
-
     def _construct_output_layers(self):
         self._head = nn.Linear(
             self._hidden_dimensions[-1], self._output_dimension, bias=self._bias
         )
         if self._unit_norm_teacher_head:
-            head_weight = random.choice([1, -1])
-            self._head.weight.data.fill_(head_weight)
+            head_norm = torch.norm(self._head.weight)
+
+            normalised_head = self._head.weight / head_norm
+
+            self._head.weight.data = normalised_head
         else:
             self._initialise_weights(self._head)
         for param in self._head.parameters():
