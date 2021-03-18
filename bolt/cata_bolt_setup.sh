b1cd6b34d68028db19607544c9402689134278fe
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-03-18 12:05:49 +0000

    remove boltt file

--- bolt/cata_bolt_setup.sh
+++
@@ -1,9 +0,0 @@
-# conda env
-source /mnt/miniconda/etc/profile.d/conda.sh
-conda activate PY3
-
-# echo python version for debugging
-python --version
-
-pip install --upgrade pip --quiet
-pip install -r bolt/bolt_requirements.txt --quiet
