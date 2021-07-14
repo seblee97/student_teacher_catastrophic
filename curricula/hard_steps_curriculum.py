d3bb422d3fd2faaf37d6810e971df6795ca1a821
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-07-14 15:06:37 +0200

    interleaving added to switch step curriculum

--- curricula/hard_steps_curriculum.py
+++ curricula/hard_steps_curriculum.py
@@ -1,7 +1,9 @@
-import numpy as np
+import itertools
 
+import numpy as np
 from curricula import base_curriculum
 from run import student_teacher_config
+from utils import custom_functions
 
 
 class HardStepsCurriculum(base_curriculum.BaseCurriculum):
@@ -11,6 +13,23 @@ class HardStepsCurriculum(base_curriculum.BaseCurriculum):
         self, config: student_teacher_config.StudentTeacherConfiguration
     ) -> None:
         self._curriculum_switch_steps = iter(config.switch_steps)
+
+        if config.interleave_period is not None:
+            assert (
+                len(config.switch_steps) < 2
+            ), "interleaving currently only implemented for single switch."
+            offset = config.switch_steps[0] + config.interleave_period
+            increments = [
+                config.interleave_duration,
+                config.interleave_period,
+            ]
+            interleave_iterator = custom_functions.create_iterator(
+                offset=offset, increments=increments
+            )
+            self._curriculum_switch_steps = itertools.chain(
+                self._curriculum_switch_steps, interleave_iterator
+            )
+
         self._next_switch_step = next(self._curriculum_switch_steps)
         super().__init__(config=config)
 
