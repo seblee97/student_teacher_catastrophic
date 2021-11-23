74d659ae34466705798a93e1009a6d948586ffd9
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-11-23 18:06:06 +0000

    add previous task parameters property

--- regularisers/base_regulariser.py
+++ regularisers/base_regulariser.py
@@ -1,7 +1,7 @@
 import abc
-import constants
 import copy
 
+import constants
 import torch
 from torch import nn
 from torch.nn import functional as F
@@ -10,32 +10,34 @@ from torch.nn import functional as F
 class BaseRegulariser(abc.ABC):
     def __init__(self, importance: float, device: str):
         self._importance = importance
-        self._device = device 
+        self._device = device
 
         self._previous_task_parameters = []
 
     def _store_previous_task_parameters(self):
-        """Store the parameters from before task switches.
-        """
+        """Store the parameters from before task switches."""
         previous_task_paramters = {}
         for n, param in copy.deepcopy(self._params).items():
             previous_task_paramters[n] = param.data.to(self._device)
 
         self._previous_task_parameters.append(previous_task_paramters)
-        
 
     @abc.abstractmethod
     def compute_first_task_importance(
-        self, 
-        student: nn.Module, 
-        previous_teacher_index: int, 
-        previous_teacher: nn.Module, 
+        self,
+        student: nn.Module,
+        previous_teacher_index: int,
+        previous_teacher: nn.Module,
         loss_function,
         data_module,
-        device: str
+        device: str,
     ):
-        pass 
+        pass
 
     @abc.abstractmethod
     def penalty(self, student: nn.Module):
         pass
+
+    @property
+    def previous_task_parameters(self):
+        return self._previous_task_parameters[-1]
