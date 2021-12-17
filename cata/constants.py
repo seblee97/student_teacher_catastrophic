4171a68692d5dccab47cbdc1e81fa5b465a27600
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-12-17 14:21:41 +0100

    loss and consolidation penalty

--- cata/constants.py
+++ cata/constants.py
@@ -193,6 +193,8 @@ STEP = "step"
 NODE_SHARING = "node_sharing"
 NUM_SHARED_NODES = "num_shared_nodes"
 CLUSTER = "cluster"
+LOSS = "loss"
+CONSOLIDATION_PENALTY = "consolidation_penalty"
 
 
 EVEN_ODD_MAPPING = {0: 0, 1: 1, 2: 0, 3: 1, 4: 0, 5: 1, 6: 0, 7: 1, 8: 0, 9: 1}
