2d8eb8a35fc552cb90f6ed6e7bf6887b842b4cab
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2022-12-08 16:59:14 +0000

    fix meta learner

--- cata/students/meta_student.py
+++ cata/students/meta_student.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+
 from cata.students import base_student
 
 
@@ -14,18 +15,21 @@ class MetaStudent(base_student.BaseStudent):
             output_layer.weight.data.fill_(1)
         else:
             if self._initialise_outputs:
-                self._initialise_weights(output_layer)
+                self._initialise_weights(output_layer, 1)
             else:
+                self._initialise_weights(output_layer)
                 nn.init.zeros_(output_layer.weight)
-                if self.bias:
+                if self._bias:
                     nn.init.zeros_(output_layer.bias)
-            self._heads.append(output_layer)
+        self._heads.append(output_layer)
 
     def _get_output_from_head(self, x: torch.Tensor) -> torch.Tensor:
         """Pass tensor through head of student (only one for meta-learning)."""
         y = self._heads[0](x)
+        if self._apply_nonlinearity_on_output:
+            y = self._nonlinear_function(y)
         if self._classification_output:
-            return self._threshold(y)
+            y = self._threshold(y)
         return y
 
     def _signal_task_boundary(self, new_task: int) -> None:
